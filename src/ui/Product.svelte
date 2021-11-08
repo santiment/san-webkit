ab6990c06229f3b2bcd0b7d485d0a12472b8c8e2
DmitriVanGuard <dimavanguard@gmail.com>
2021-11-08 22:15:04 +0300

    Product component

---
+++ src/ui/Product.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  let className = ''
+  export { className as class }
+  export let title: string
+</script>
+
+<a href="/" class="body-2 txt-m title row v-center {className}">
+  <img
+    src="{process.env.MEDIA_PATH}/icons/san-logo.svg"
+    alt="SAN"
+    class="mrg-s mrg--r"
+  />
+  {title}
+</a>
+
+<style>
+  a {
+    color: var(--black);
+  }
+
+  img {
+    width: 32px;
+    height: 32px;
+  }
+</style>
