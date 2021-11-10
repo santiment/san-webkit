9a5237010236f9fdbe62cc00bb2cb8b095988904
DmitriVanGuard <dimavanguard@gmail.com>
2021-11-10 17:08:59 +0300

    Table minRows and loading indicator

--- src/ui/Table.svelte
+++ src/ui/Table.svelte
@@ -4,6 +4,27 @@
   export let columns
   export let items
   export let keyProp
+  export let minRows = undefined
+  export let isLoading = false
+
+  $: rowsPadding = minRows ? getMinRows(items.length, columns.length) : ''
+
+  const COLUMN_PADDING = '<td></td>'
+  function getMinRows(itemsLength, columnsLength) {
+    const rowsAmount = minRows - itemsLength
+    if (rowsAmount <= 0) return ''
+
+    const columnsPadding = ''.padStart(
+      columnsLength * COLUMN_PADDING.length,
+      COLUMN_PADDING
+    )
+
+    let rows = ''
+    for (let i = 0; i < rowsAmount; i++) {
+      rows += '<tr>' + columnsPadding + '</tr>'
+    }
+    return rows
+  }
 </script>
 
 <table class="panel {className}">
@@ -20,7 +41,7 @@
         {#each columns as { title, className, format, Component } (title)}
           <td class={className || ''}>
             {#if Component}
-              <svelte:component this={Component} {item} {...item} />
+              <svelte:component this={Component} {item} />
             {:else}
               {format(item, i)}
             {/if}
@@ -28,7 +49,15 @@
         {/each}
       </tr>
     {/each}
+    {#if rowsPadding}
+      {@html rowsPadding}
+    {/if}
   </tbody>
+  <caption>
+    {#if isLoading}
+      <div class="loader">Loading...</div>
+    {/if}
+  </caption>
 </table>
 
 <style>
@@ -45,6 +74,7 @@
     width: 100%;
     border-spacing: 0;
     text-align: left;
+    position: relative;
   }
 
   table :global(th),
@@ -68,5 +98,16 @@
 
   table :global(tbody tr:hover td) {
     background: var(--athens);
+  }
+
+  .loader {
+    padding: 10px 20px;
+    color: var(--casper);
+    background: var(--athens);
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate3d(-50%, -50%, 0);
+    border-radius: 4px;
   }
 </style>
