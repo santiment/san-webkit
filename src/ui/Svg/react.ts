eedb9ab2069c30b0837cd7e9dcc0712717adf31f
DmitriVanGuard <dimavanguard@gmail.com>
2021-12-02 13:36:04 +0300

    Svg and Tooltip react components

---
+++ src/ui/Svg/react.ts
@@ -0,0 +1,33 @@
+import { createElement, useState, useEffect, useRef } from 'react'
+import SvelteSvg from '../Svg.svelte'
+
+const Svg = ({ id, illus, className, w, h }) => {
+  const [mounted, setMounted] = useState<boolean | null>(true)
+  const ref = useRef<HTMLElement>(null)
+
+  useEffect(() => {
+    const node = ref.current
+    if (!node) return
+
+    const fragment = document.createDocumentFragment()
+    const svelte = new SvelteSvg({
+      target: fragment as any,
+      props: {
+        id,
+        illus,
+        className,
+        w,
+        h,
+        $$scope: { ctx: {} },
+      },
+    })
+
+    node.parentNode.insertBefore(fragment, node)
+    setMounted(null)
+    return () => svelte.$destroy()
+  }, [])
+
+  return mounted && createElement('p', { ref, className: 'hide' })
+}
+
+export default Svg
---
+++ src/ui/Tooltip/react.ts
@@ -0,0 +1,82 @@
+import type { SvelteComponent } from 'svelte'
+import {
+  Fragment,
+  createElement,
+  cloneElement,
+  useState,
+  useEffect,
+  useRef,
+} from 'react'
+import ReactDOM from 'react-dom'
+import SvelteTooltip from '../Tooltip.svelte'
+
+export const Tooltip = ({
+  trigger,
+  tooltip,
+  className,
+  activeClass,
+  dark,
+  position,
+  align,
+  duration,
+  openDelay,
+  closeTimeout,
+  isEnabled = true,
+}) => {
+  const [tooltipTarget, setTooltipTarget] = useState<HTMLElement | null>()
+  const svelteRef = useRef<SvelteComponent | null>(null)
+  const ref = useRef<HTMLElement>(null)
+
+  useEffect(() => {
+    const triggerNode = ref.current
+    if (!triggerNode) return
+
+    const createTooltipSlot = () => ({
+      c() {},
+      m(target) {
+        setTooltipTarget(target)
+      },
+      d(detaching) {
+        if (detaching) setTooltipTarget(null)
+      },
+    })
+
+    const svelte = new SvelteTooltip({
+      target: triggerNode.parentNode as HTMLElement,
+      props: {
+        position,
+        align,
+        duration,
+        openDelay,
+        dark,
+        closeTimeout,
+        activeClass,
+        class: className,
+        trigger: triggerNode,
+        $$slots: {
+          tooltip: [createTooltipSlot],
+        },
+        $$scope: { ctx: {} },
+      },
+    })
+
+    svelteRef.current = svelte
+    return () => svelte.$destroy()
+  }, [])
+
+  useEffect(() => {
+    const svelte = svelteRef.current
+    if (!svelte) return
+
+    svelte.$set({ isEnabled })
+  }, [isEnabled])
+
+  return createElement(
+    Fragment,
+    null,
+    cloneElement(trigger, { ref }),
+    tooltipTarget && ReactDOM.createPortal(tooltip, tooltipTarget)
+  )
+}
+
+export default Tooltip
