574235c9ff65de2bd8d6c7a5d695c819e647e78d
DmitriVanGuard <dimavanguard@gmail.com>
2021-12-20 11:48:15 +0300

    Svg optional 'w' prop

--- src/ui/Svg/index.svelte
+++ src/ui/Svg/index.svelte
@@ -4,7 +4,7 @@
   let className = ''
   export { className as class }
   export let id: Props['id']
-  export let w: Props['w']
+  export let w: Props['w'] = undefined
   export let h: Props['h'] = w
   export let illus = false
 
