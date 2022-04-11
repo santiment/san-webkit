2ca4af8ab5e9f23dd33f4b9a222bfbcc662b1465
SaeidR <saeidr@evenset.com>
2022-04-11 05:51:05 -0400

    range component added, new classes added

---
+++ src/ui/Range.svelte
@@ -0,0 +1,30 @@
+<script>
+    export let items = []
+    export let hasAllWithLabel;
+    export let onChange = (index, value) => {}
+    let className = ''
+    export {className as class}
+    export let style;
+
+    let index = 0;
+    $: if (hasAllWithLabel) items = [...items, hasAllWithLabel]
+
+    function clickHandler() {
+        index += 1;
+        if (index + 1 > items.length) {
+            index = 0
+        }
+        onChange(index, items[index])
+    }
+</script>
+
+<button class="btn btn-2 {className}" on:click={clickHandler} {style}>
+    {items[index]}
+</button>
+
+<style>
+button {
+    --v-padding: 6px;
+    --h-padding: 12px;
+}
+</style>
---
+++ stories/Range.stories.svelte
@@ -0,0 +1,16 @@
+<script>
+import Index from '@/ui/Svg/index.svelte';
+
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+
+  import Range from '../src/ui/Range.svelte'
+</script>
+
+<Meta title="Example/Range" component={Range} />
+
+<Template let:args>
+  <Range {...args} style="width: 48px;" items={['7', '12h', '24d']} onChange={(Index, value) => console.log({index: value})} />
+</Template>
+
+<Story name="Default" />
+
