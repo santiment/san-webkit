8f8d82400979a3420f037889c3ccdae74eb9985c
SaeidR <saeidr@evenset.com>
2022-04-11 08:11:58 -0400

    PR issue resolved

--- src/ui/Range.svelte
+++ src/ui/Range.svelte
@@ -1,12 +1,9 @@
 <script>
+    let className = ''
+    export {className as class}
     export let items = []
-    export let hasAllWithLabel;
     export let selectedIndex = 0
     export let onChange = value => {}
-    export {className as class}
-
-    let className = ''
-    items = hasAllWithLabel ? [...items, hasAllWithLabel] : items
 
     function onClick() {
         selectedIndex = ++selectedIndex % items.length
@@ -17,9 +14,3 @@
 <button class="btn-2 btn--s {className}" on:click={onClick}>
     {items[selectedIndex]}
 </button>
-
-<style>
-button {
-    --v-padding: 6px;
-}
-</style>
