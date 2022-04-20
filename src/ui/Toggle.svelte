98beb8472a5dd7af22cadbb7efd835f897970da4
DmitriVanGuard <dimavanguard@gmail.com>
2022-04-20 12:24:56 +0300

    Toggle styles

--- src/ui/Toggle.svelte
+++ src/ui/Toggle.svelte
@@ -2,22 +2,21 @@
   let className = ''
   export { className as class }
   export let isActive = false
+  export let disabled = false
 </script>
 
-<div class="toggle {className}" on:click class:isActive>
+<div class="toggle btn relative {className}" on:click class:isActive class:disabled>
   <img alt="Toggle" src="{process.env.ICONS_PATH}/toggle.svg" />
 </div>
 
 <style>
   .toggle {
-    user-select: none;
-    position: relative;
-    cursor: pointer;
     display: inline-block;
-    background: var(--casper);
     line-height: 0;
     border-radius: 10px;
     transition: background 185ms;
+    --bg: var(--casper);
+    --bg-hover: var(--waterloo);
   }
   .toggle::before {
     display: inline-block;
@@ -33,9 +32,15 @@
   }
 
   .isActive {
-    background: var(--green);
+    --bg: var(--green);
+    --bg-hover: var(--green-hover);
   }
   .isActive::before {
     transform: translateX(calc(100% + 2px));
+  }
+
+  .disabled {
+    background: var(--porcelain) !important;
+    pointer-events: none !important;
   }
 </style>
