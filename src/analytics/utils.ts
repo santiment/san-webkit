92eba1a16018bddbf492cb3c4cfacd0dbf3a9d66
Igor Ko <46782114+Woafflation@users.noreply.github.com>
2022-05-30 16:41:16 +0300

    Updated init twitter

--- src/analytics/utils.ts
+++ src/analytics/utils.ts
@@ -3,10 +3,16 @@ export const scriptTag = () => document.createElement('script')
 export function newHeadScript(
   content: string | undefined,
   options?: { [key: string]: any },
+  onload?: () => void,
 ): HTMLScriptElement {
   const script = scriptTag()
   if (content) script.text = content
   Object.assign(script, options)
+  if (onload) {
+    script.onload = function () {
+      onload()
+    }
+  }
   document.head.appendChild(script)
   return script
 }
