109ce5880d7ee1f56af774ed4d9e2f506650d684
SaeidR <saeidr@evenset.com>
2022-06-14 05:14:53 -0400

    profile empty caption bug fixed

--- src/ui/Profile/Names.svelte
+++ src/ui/Profile/Names.svelte
@@ -7,19 +7,22 @@
   export let followers: number | undefined = undefined
 
   $: ({ username, email } = user)
+  $: hasCaption = (followers && followers >= 0) || email
 </script>
 
 <Profile {user} class="txt-m {className}">
   <svelte:fragment slot="name">
     <div class="">
       <div class="caption body-3  txt-r">@{username}</div>
-      <div class="caption txt-r c-waterloo">
-        {#if followers && followers >= 0}
-          {followers} Followers
-        {:else}
-          {email}
-        {/if}
-      </div>
+      {#if hasCaption}
+        <div class="caption txt-r c-waterloo">
+          {#if followers && followers >= 0}
+            {followers} Followers
+          {:else}
+            {email}
+          {/if}
+        </div>
+      {/if}
     </div>
   </svelte:fragment>
 </Profile>
