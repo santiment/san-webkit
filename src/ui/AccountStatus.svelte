0155a62f611d6da227ba52613543c91999cd3b92
SaeidR <saeidr@evenset.com>
2022-06-15 04:53:37 -0400

    PR comments applied

--- src/ui/AccountStatus.svelte
+++ src/ui/AccountStatus.svelte
@@ -1,14 +1,20 @@
+<script lang="ts" context="module">
+  export enum AccountDropdownVariant {
+    Version1 = 1,
+    Version2 = 2,
+  }
+</script>
+
 <script>
   import { customerData$ } from '@/stores/user'
   import { subscription$ } from '@/stores/subscription'
   import { PlanName } from '@/utils/plans'
-  import { AccountDropdown } from '@/utils/enums'
 
   export let currentUser
-  export let variant = AccountDropdown.Version1
+  export let variant = AccountDropdownVariant.Version1
 
   $: ({ isEligibleForTrial, annualDiscount } = $customerData$)
-  $: trialSpecialOfferPercent =
+  $: specialOfferPercent =
     (annualDiscount &&
       annualDiscount.isEligible &&
       annualDiscount.discount &&
@@ -21,12 +27,12 @@
 {#if currentUser}
   {#if currentPlanName && currentPlanName.toUpperCase().includes('PRO')}
     <div class="row v-center">
-      {#if trialSpecialOfferPercent > 0}
+      {#if specialOfferPercent > 0}
         <a
           href="https://app.santiment.net/pricing"
-          class="btn-1 btn-2 btn--orange mrg--r mrg-m"
+          class="btn-2 btn-1 btn--orange mrg-m mrg--r"
           on:click={window.__onLinkClick}
-          >Special offer! {trialSpecialOfferPercent}% OFF
+          >Special offer! {specialOfferPercent}% OFF
         </a>
       {/if}
       <a
@@ -41,15 +47,15 @@
         href="https://app.santiment.net/pricing"
         class="btn-1 btn--orange"
         on:click={window.__onLinkClick}
-        >{variant === AccountDropdown.Version1 ? 'Start Free 14-day Trial' : 'Upgrade'}</a>
+        >{variant === AccountDropdownVariant.Version1 ? 'Start Free 14-day Trial' : 'Upgrade'}</a>
     </div>
-  {:else if trialSpecialOfferPercent > 0}
-    {#if variant === AccountDropdown.Version1}
+  {:else if specialOfferPercent > 0}
+    {#if variant === AccountDropdownVariant.Version1}
       <div class="caption c-waterloo">
         <a
           href="https://app.santiment.net/pricing"
           class="btn-1 btn-2 btn--orange"
-          on:click={window.__onLinkClick}>Special offer! {trialSpecialOfferPercent}% OFF</a>
+          on:click={window.__onLinkClick}>Special offer! {specialOfferPercent}% OFF</a>
       </div>
     {/if}
   {:else}
--- src/utils/enums.ts
+++
@@ -1,4 +0,0 @@
-export enum AccountDropdown {
-  Version1 = 1,
-  Version2 = 2,
-}
