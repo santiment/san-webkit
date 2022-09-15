a8c1c0c2f105e39deb8c721f4285af66970c6604
SaeidR <saeid.r@santiment.net>
2022-09-15 06:22:32 -0400

    bugs fixed, more tests added

--- src/ui/AnimatedLink/index.svelte
+++ src/ui/AnimatedLink/index.svelte
@@ -1,4 +1,6 @@
 <script>
+  import Svg from '@/ui/Svg/svelte'
+
   let className = ''
   export { className as class }
   export let hasArrow = false
@@ -8,41 +10,28 @@
   export let label
 </script>
 
-<a {href} {target} {rel} class="btn-0 {className} animhover" {...$$props}>
-  <div>
-    <span>{label}</span>
+<a {href} {target} {rel} {...$$props} class="btn-0 {className} animhover">
+  <div class="row v-center">
+    <span class="relative">{label}</span>
     {#if hasArrow}
-      <img
-        src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACgAAAAUCAYAAAD/Rn+7AAAACXBIWXMAACxLAAAsSwGlPZapAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAFdSURBVHgBzZZfTsJAEMZndjkAieBzb2B8FhRvwA30AID1BNYT0BTfjTfwBlTqAfAG+ERiMOl7ZdcZQkmzFHjb3e+pO9tJfvvnmx0Ei2rNkjEghvQ5/xPYz68G36dyECyJ4G4ILi3HWuuFROj/dEdfx/IEOBIiBgpw3s4mD8f+swa4uh590LbFZlwDxK1s8nQoz9oRl9rCRHsTBE+LeDTD1gFZfKy8czVTe+ZxAsg6z5ILjeKdzBJU42yetRS9EtKZSdi9sihuySyLapzNI9cq5QVsxmefL3eoVEwzTfBMGvFegFKRj3AsJOMI9BSORWw5A4bmPfBEuUIInbmY1ZyOAynllI1RjfOGoVabZ9BpmVEaUvP+M1wDobd0WWa4cvA7XGPOtCiKy6XLQu31U9eeJa9c22qmolV3+FyX46wf3Con4PC3M3g7lNcAR9o5tTP0o2E1+sGUnXqqm2b9A6Akjs5t3qqGAAAAAElFTkSuQmCC"
-        alt="right arrow"
-      />
+      <Svg id="pointer" w={14} class="$style.svg" />
     {/if}
   </div>
 </a>
 
 <style>
   .animhover {
-    padding: 0 4px;
+    display: inline-block;
   }
 
-  .animhover div {
-    display: flex;
-    align-items: center;
-  }
-
-  .animhover img {
-    width: 14px;
-    margin-left: 4px;
+  .animhover .svg {
+    margin-left: 2px;
     transform: translateX(-2px);
     transition: transform 0.3s;
   }
 
-  .animhover:hover img {
+  .animhover:hover .svg {
     transform: translateX(0);
-  }
-
-  .animhover span {
-    position: relative;
   }
 
   .animhover span:after {
@@ -52,7 +41,7 @@
     bottom: 0;
     left: 0;
     width: 0;
-    height: 1px;
+    height: var(--link-underline-height, 1px);
     transform-origin: bottom left;
     transition: width 0.3s;
   }
--- stories/AnimatedLink/AnimatedLink.stories.svelte
+++ stories/AnimatedLink/AnimatedLink.stories.svelte
@@ -6,16 +6,38 @@
 <Meta title="Example/AnimatedLinks" />
 
 <Story name="AnimatedLinks">
-  <div class="row">
+  <div>
     This is a <AnimatedLink href="/" label="Link" /> without image
   </div>
-  <div class="row">
+  <div>
     This is a <AnimatedLink href="/" label="Link" hasArrow /> with arrow image
+  </div>
+  <div>
+    <p class="body-1">
+      We bring together everything that you can use to make your next action. Datasets, <AnimatedLink
+        href="https://app.santiment.net/screener/new"
+        label="screeners"
+      />, <AnimatedLink href="https://app.santiment.net/watchlists" label="watchlists" />, <AnimatedLink
+        href="https://app.santiment.net/alerts"
+        label="alerts"
+      />, flexible <AnimatedLink href="https://app.santiment.net/charts" label="chart layouts" />, <AnimatedLink
+        href="https://app.santiment.net/dashboards"
+        label="dashboards"
+      />, <AnimatedLink href="https://insights.santiment.net/" label="insights" /> from the community
+      and the San analytics team.
+    </p>
+  </div>
+  <div>
+    <AnimatedLink
+      href="https://app.santiment.net/s/1PcSbMgY"
+      label="Get to know social trends"
+      hasArrow
+    />
   </div>
 </Story>
 
 <style>
-  div.row {
+  div {
     margin: 20px;
   }
 </style>
