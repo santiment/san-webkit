cf8693f005aab7ed3aeb5637ca51ee105b6ced8a
DmitriVanGuard <dimavanguard@gmail.com>
2022-09-28 17:04:58 +0300

    Skeleton styles updated

--- src/ui/Skeleton.svelte
+++ src/ui/Skeleton.svelte
@@ -5,7 +5,7 @@
 </script>
 
 <slot />
-{#if isActive}<div class="row" out:fade />{/if}
+{#if isActive}<div class="skeleton" out:fade />{/if}
 
 <style>
   div {
@@ -15,26 +15,5 @@
     width: 100%;
     height: 100%;
     z-index: 2;
-    overflow: hidden;
-    border-radius: 4px;
-    background: var(--porcelain);
-    /* background-size: 400% 100%; */
-  }
-
-  div::before {
-    content: '';
-    width: 400%;
-    height: 100%;
-    background: linear-gradient(90deg, var(--porcelain) 0, var(--athens) 30%, var(--porcelain) 50%);
-    animation: move-gradient 1.2s cubic-bezier(1, 0.54, 0.41, 0.92) infinite;
-  }
-
-  @keyframes move-gradient {
-    0% {
-      transform: translateX(-85%);
-    }
-    100% {
-      transform: translateX(100%);
-    }
   }
 </style>
