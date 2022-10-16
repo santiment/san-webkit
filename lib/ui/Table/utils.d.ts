b90564a81ea7c627597c5942d62aa2e71698309b
DmitriVanGuard <dimavanguard@gmail.com>
2022-10-16 12:49:25 +0300

    Library release

--- lib/ui/Table/utils.d.ts
+++ lib/ui/Table/utils.d.ts
@@ -4,14 +4,17 @@ export declare type Item = {
 };
 export declare type Sorter = (a: Item, b: Item) => number;
 export declare type SortAccessor = (item: Item) => number;
+export declare type Format<T = Item> = (item: T, i: number, value?: any) => number | string;
 export declare type Column<T = Item> = {
     title: string;
-    format: (item: T, i: number, value?: T) => number | string;
     className?: string;
     sortAccessor?: SortAccessor;
-    Component?: SvelteComponentDev;
     Header?: SvelteComponentDev;
     valueKey?: string;
     isSortable?: boolean;
-};
+} & ({
+    format: Format<T>;
+} | {
+    Component: SvelteComponentDev;
+});
 export declare function getMinRows(minRows: undefined | number, itemsLength: number, columnsLength: number): string;
