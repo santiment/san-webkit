91d2a144196c85f3a02dde8b4b270eff04d45796
Igor <ihqrk@pm.me>
2022-10-27 20:13:26 +0300

    Added Clue component

---
+++ src/ui/Halloween/Clue.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+</script>
+
+<div class="divider fluid mrg-xxl mrg--t mrg--b" />
+<div class="clue-wrapper">
+  <p class="mrg-l mrg--b">
+    We’ve hidden one more treasure somewhere on the Sanbase. Get through the riddle and find a
+    Special offer if you want to double your discount!
+  </p>
+  <div class="clue">
+    <p class="txt-m">Here is the clue:</p>
+    <p>Try seeking information about SAN token to find an extra discount option</p>
+  </div>
+</div>
+
+<style>
+  .divider {
+    height: 1px;
+    background-color: var(--porcelain);
+  }
+
+  .clue-wrapper {
+    padding: 0 24px;
+  }
+
+  .clue {
+    border: 1px solid var(--orange);
+    border-radius: 6px;
+    padding: 12px 44px;
+  }
+</style>
--- src/ui/Halloween/Dialog.svelte
+++ src/ui/Halloween/Dialog.svelte
@@ -11,6 +11,7 @@
   import { halloweenData$ } from '@/stores/halloween'
   import FlamePumpkin from './FlamePumpkin.svelte'
   import DiscountCode from './DiscountCode.svelte'
+  import Clue from './Clue.svelte'
 
   let closeDialog
   let title = 'Enjoy your 27% off discount!'
@@ -87,17 +88,7 @@
       </button>
     {/if}
     {#if isClueShown}
-      <div class="divider fluid mrg-xxl mrg--t mrg--b" />
-      <div class="clue-wrapper">
-        <p class="mrg-l mrg--b">
-          We’ve hidden one more treasure somewhere on the Sanbase. Get through the riddle and find a
-          Special offer if you want to double your discount!
-        </p>
-        <div class="clue">
-          <p class="txt-m">Here is the clue:</p>
-          <p>Try seeking information about SAN token to find an extra discount option</p>
-        </div>
-      </div>
+      <Clue />
     {/if}
   </div>
 </Dialog>
@@ -147,20 +138,5 @@
     --color-hover: var(--casper);
     max-width: 228px;
     text-decoration: underline;
-  }
-
-  .divider {
-    height: 1px;
-    background-color: var(--porcelain);
-  }
-
-  .clue-wrapper {
-    padding: 0 24px;
-  }
-
-  .clue {
-    border: 1px solid var(--orange);
-    border-radius: 6px;
-    padding: 12px 44px;
   }
 </style>
