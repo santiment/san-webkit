32d629769ecd688060e3bd78321598a8f1d6c4e3
DmitriVanGuard <dimavanguard@gmail.com>
2022-12-15 12:45:59 +0300

    Initial dialog setup

---
+++ src/ui/ChristmasNFTDialog/Dialog.svelte
@@ -0,0 +1,34 @@
+<script context="module" lang="ts">
+  import { dialogs } from '@/ui/Dialog'
+  import ChristmasNFTDialog from './Dialog.svelte'
+
+  export const showChristmasNFTDialog = (props) =>
+    dialogs.showOnce(ChristmasNFTDialog, Object.assign({ strict: true }, props))
+</script>
+
+<script lang="ts">
+  import Svg from '@/ui/Svg/svelte'
+  import Dialog from '@/ui/Dialog'
+</script>
+
+<Dialog {...$$props} noTitle class="body-2">
+  <h2 class="h3 txt-m txt-center mrg-xl mrg--b">San NFT Christmas Game!</h2>
+  <p class="txt-center mrg-xxl mrg--b">
+    We are hosting a Christmas event to reward the best Sanbase crypto analysts in our community!
+    Participate in our Insight writing competition, and get 1 of 10 NFT’s that unlock three months
+    of Sanbase PRO access alongside a lifetime 18% discount.
+  </p>
+
+  <button class="btn-1 btn--l">Start the Game</button>
+</Dialog>
+
+<style lang="scss">
+  p {
+    max-width: 490px;
+  }
+
+  button {
+    margin: 0 auto;
+    padding: 8px 44px;
+  }
+</style>
---
+++ stories/ChristmasNFT/ChristmasNFTDialog.stories.svelte
@@ -0,0 +1,18 @@
+<script>
+  import { Meta, Template, Story } from '@storybook/addon-svelte-csf'
+  import Dialogs from '@/ui/Dialog/Dialogs.svelte'
+  import OnMount from '../PaymentDialog/OnMount.svelte'
+  import { showChristmasNFTDialog } from '@/ui/ChristmasNFTDialog/Dialog.svelte'
+</script>
+
+<Meta title="Example/ChristmasNFTDialog" />
+
+<Template>
+  <button on:click={showChristmasNFTDialog}>Show dialog</button>
+</Template>
+
+<Story name="Welcome dialog">
+  <OnMount run={() => showChristmasNFTDialog()} />
+</Story>
+
+<Dialogs />
