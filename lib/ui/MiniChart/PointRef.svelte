17472e8ad8113570ef55e742f9bb919bed196f6b
DmitriVanGuard <dimavanguard@gmail.com>
2022-12-21 18:00:53 +0300

    Library release

--- lib/ui/MiniChart/PointRef.svelte
+++ lib/ui/MiniChart/PointRef.svelte
@@ -1,44 +1,33 @@
-<script context="module">
-  export function getRefCoordinates(
-    data: any[],
-    points: string[],
-    target: number,
-    datetimeKey = 'd',
-  ) {
-    if (!points || points.length < 3) {
-      return {
-        index: -1,
-        coordinates: [],
-      }
-    }
-
-    const { length } = data
-    const first = +new Date(data[0][datetimeKey])
-    const last = +new Date(data[length - 1][datetimeKey])
-
-    let index = Math.round(map(target, first, last, 0, length))
-    if (index === length) index -= 1
-
-    const point = points[index]
-
+<script context="module">export function getRefCoordinates(data, points, target, datetimeKey = 'd') {
+  if (!points || points.length < 3) {
     return {
-      index,
-      coordinates: point ? point.split(',') : [],
-    }
+      index: -1,
+      coordinates: []
+    };
   }
-</script>
 
-<script lang="ts">
-  import { map } from './../../utils'
+  const {
+    length
+  } = data;
+  const first = +new Date(data[0][datetimeKey]);
+  const last = +new Date(data[length - 1][datetimeKey]);
+  let index = Math.round(map(target, first, last, 0, length));
+  if (index === length) index -= 1;
+  const point = points[index];
+  return {
+    index,
+    coordinates: point ? point.split(',') : []
+  };
+}</script>
 
-  export let target: number
-  export let data: any[]
-  export let points: string[]
-  export let stroke = 'var(--casper)'
-  export let datetimeKey = 'd'
+<script>import { map } from './../../utils';
+export let target;
+export let data;
+export let points;
+export let stroke = 'var(--casper)';
+export let datetimeKey = 'd';
 
-  $: [cx, cy] = getRefCoordinates(data, points, target, datetimeKey).coordinates
-</script>
+$: [cx, cy] = getRefCoordinates(data, points, target, datetimeKey).coordinates;</script>
 
 <circle {cx} {cy} {stroke} r="3.5" />
 
