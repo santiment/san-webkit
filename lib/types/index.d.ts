634111f3ad44288e500bbf6162a974d1590dbd5b
DmitriVanGuard <dimavanguard@gmail.com>
2023-01-10 16:28:30 +0300

    Library release

--- lib/types/index.d.ts
+++ lib/types/index.d.ts
@@ -30,6 +30,7 @@ interface Window {
   showCancelSubscriptionDialog?: (e: MouseEvent) => void
   twq?: any
   __SAPPER__?: any
+  __SESSION__?: any
 
   amplitude?: {
     track: (action: string, eventProperties: { [key: string]: any }) => void
