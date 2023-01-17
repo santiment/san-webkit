eb47bbeb253cc47ce7ccf98a333e88509a33d435
DmitriVanGuard <dimavanguard@gmail.com>
2023-01-17 12:14:03 +0300

    Library release

--- lib/ui/LoginPrompt/Google.svelte
+++ lib/ui/LoginPrompt/Google.svelte
@@ -1,11 +1,12 @@
 <script>import { LoginType, trackLoginStart } from './../../analytics/events/general';
 import { trackSignupStart } from './../../analytics/events/onboarding';
+import { getOAuthLink } from './../../utils/auth';
 import Option from './Option.svelte';</script>
 
 <Option
   {...$$props}
   title="Google"
-  href="{process.env.BACKEND_URL}/auth/google"
+  href={getOAuthLink('google')}
   icon="google"
   on:click={() => {
     if ($$props.isSignUp) {
--- lib/ui/LoginPrompt/Twitter.svelte
+++ lib/ui/LoginPrompt/Twitter.svelte
@@ -1,11 +1,12 @@
 <script>import { LoginType, trackLoginStart } from './../../analytics/events/general';
 import { trackSignupStart } from './../../analytics/events/onboarding';
+import { getOAuthLink } from './../../utils/auth';
 import Option from './Option.svelte';</script>
 
 <Option
   {...$$props}
   title="Twitter"
-  href="{process.env.BACKEND_URL}/auth/twitter"
+  href={getOAuthLink('twitter')}
   icon="twitter"
   on:click={() => {
     if ($$props.isSignUp) {
