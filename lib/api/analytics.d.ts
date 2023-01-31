15b76cd3b9de37cb5003c5540114a279c70b279b
DmitriVanGuard <dimavanguard@gmail.com>
2023-01-31 10:31:48 +0300

    Library release

--- lib/api/analytics.d.ts
+++ lib/api/analytics.d.ts
@@ -6,5 +6,5 @@ export declare const queryCurrentUser: () => Promise<CurrentUser>;
 declare type EventData = {
     [key: string]: string | number;
 };
-export declare const trackSanEvent: (event_name: string, created_at: Date, metadata: EventData) => Promise<SAN.API.QueryBase | undefined>;
+export declare const trackSanEvent: (event_name: string, created_at: Date, metadata: EventData) => Promise<SAN.API.QueryBase>;
 export {};
