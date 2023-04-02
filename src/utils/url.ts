97559a2f1a3beb1962073b28449b1e22604c120d
DmitriVanGuard <dimavanguard@gmail.com>
2023-04-02 21:36:25 +0200

    Exporting SEO normalizer

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,19 +1,16 @@
 const NON_WORDS_REGEX = /[\W_]+/g
 const SPACES_REGEX = /\s/g
 
+export const sluggify = (title: string) =>
+  encodeURIComponent(
+    title.replace(NON_WORDS_REGEX, ' ').trim().replace(SPACES_REGEX, '-').toLowerCase(),
+  )
+
 export const getIdFromSEOLink = (link: string): number =>
   parseInt(link.slice(link.lastIndexOf('-') + 1), 10)
 
 export const getSEOLinkFromIdAndTitle = (id: string | number, title?: string | null): string =>
-  title
-    ? encodeURIComponent(
-        `${title
-          .replace(NON_WORDS_REGEX, ' ')
-          .trim()
-          .replace(SPACES_REGEX, '-')
-          .toLowerCase()}-${id}`,
-      )
-    : `${id}`
+  title ? sluggify(title) + `-${id}` : `${id}`
 
 export function parse(queryString: string) {
   const params = new URLSearchParams(queryString)
