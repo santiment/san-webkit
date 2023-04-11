44d53d14ece2658c1ad6e495c583a59e40838708
DmitriVanGuard <dimavanguard@gmail.com>
2023-04-11 12:48:38 +0200

    Library release

--- lib/ui/Dialog/ctx.d.ts
+++ lib/ui/Dialog/ctx.d.ts
@@ -1,21 +1,27 @@
 import { DialogLock } from './types';
 export declare const CTX = "Dialog";
-export type DialogCtxType = ReturnType<typeof newDialogCtx>;
-export declare const newDialogCtx: (DialogPromise: SAN.Dialog.Promise, closeDialog?: (skipLockChecks?: boolean) => void) => {
+export type DialogCtxType<T = unknown> = ReturnType<typeof newDialogCtx<T>>;
+export declare const newDialogCtx: <T = unknown>(DialogPromise: SAN.Dialog.Promise, closeDialog?: (skipLockChecks?: boolean) => void) => {
     close: (skipLockChecks?: boolean) => void;
     lock: () => DialogLock;
     lockWarn: () => DialogLock;
     unlock: () => DialogLock;
+    resolve: (value: T) => void;
+    reject: <T_1>(reason?: T_1 | undefined) => void;
 };
 export declare const setDialogCtx: (DialogCtx: DialogCtxType) => {
     close: (skipLockChecks?: boolean) => void;
     lock: () => DialogLock;
     lockWarn: () => DialogLock;
     unlock: () => DialogLock;
+    resolve: (value: unknown) => void;
+    reject: <T>(reason?: T | undefined) => void;
 };
-export declare const getDialogCtx: () => {
+export declare const getDialogCtx: <T>() => {
     close: (skipLockChecks?: boolean) => void;
     lock: () => DialogLock;
     lockWarn: () => DialogLock;
     unlock: () => DialogLock;
+    resolve: (value: T) => void;
+    reject: <T_1>(reason?: T_1 | undefined) => void;
 };
