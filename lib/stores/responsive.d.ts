51783ef8be086ed29e7e2b6954b3bde1c9862ef9
DmitriVanGuard <dimavanguard@gmail.com>
2023-04-22 14:57:54 +0200

    Library release

--- lib/stores/responsive.d.ts
+++ lib/stores/responsive.d.ts
@@ -1,2 +1,57 @@
+import { Device } from './../responsive';
 export declare const IsMobile: import("svelte/store").Writable<boolean>;
 export declare const IsPhone: import("svelte/store").Writable<boolean>;
+export type DeviceInfoType = ReturnType<typeof getDeviceInfo>;
+export declare function getDeviceInfo(type: Device): {
+    type: Device;
+    isMobile: boolean;
+    isDesktop: boolean;
+};
+export declare function Device$$(device: DeviceInfoType): {
+    device$: {
+        onResize(): void;
+        set(this: void, value: {
+            type: Device;
+            isMobile: boolean;
+            isDesktop: boolean;
+        }): void;
+        update(this: void, updater: import("svelte/store").Updater<{
+            type: Device;
+            isMobile: boolean;
+            isDesktop: boolean;
+        }>): void;
+        subscribe(this: void, run: import("svelte/store").Subscriber<{
+            type: Device;
+            isMobile: boolean;
+            isDesktop: boolean;
+        }>, invalidate?: ((value?: {
+            type: Device;
+            isMobile: boolean;
+            isDesktop: boolean;
+        } | undefined) => void) | undefined): import("svelte/store").Unsubscriber;
+    };
+};
+export declare const getDevice$Ctx: () => {
+    device$: {
+        onResize(): void;
+        set(this: void, value: {
+            type: Device;
+            isMobile: boolean;
+            isDesktop: boolean;
+        }): void;
+        update(this: void, updater: import("svelte/store").Updater<{
+            type: Device;
+            isMobile: boolean;
+            isDesktop: boolean;
+        }>): void;
+        subscribe(this: void, run: import("svelte/store").Subscriber<{
+            type: Device;
+            isMobile: boolean;
+            isDesktop: boolean;
+        }>, invalidate?: ((value?: {
+            type: Device;
+            isMobile: boolean;
+            isDesktop: boolean;
+        } | undefined) => void) | undefined): import("svelte/store").Unsubscriber;
+    };
+};
---
+++ lib/ui/OnlyOnDevice.svelte
@@ -0,0 +1,18 @@
+<script>import { getDevice$Ctx } from './../stores/responsive';
+export let desktop = false;
+export let laptop = false;
+export let tablet = false;
+export let phone = false;
+const targets = {
+  desktop,
+  laptop,
+  tablet,
+  phone
+};
+const {
+  device$
+} = getDevice$Ctx();</script>
+
+{#if targets[$device$.type]}
+  <slot />
+{/if}
