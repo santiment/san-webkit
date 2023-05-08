0db6bbdc5aa62f9c3f46f8cfb0fb0ae30bb76475
DmitriVanGuard <dimavanguard@gmail.com>
2023-05-08 12:41:17 +0300

    Library release

---
+++ lib/ui/Tooltip/2.svelte.d.ts
@@ -0,0 +1,34 @@
+import { SvelteComponentTyped } from "svelte";
+import type { Placement } from '@floating-ui/dom';
+declare const __propDef: {
+    props: {
+        class?: string | undefined;
+        dark?: boolean | undefined;
+        isOpened?: boolean | undefined;
+        style?: string | undefined;
+        position?: Placement | undefined;
+        activeClass?: string | undefined;
+        on?: "click" | "mouseenter" | undefined;
+        duration?: number | undefined;
+        openDelay?: number | undefined;
+        closeTimeout?: number | undefined;
+        margin?: number | undefined;
+    };
+    events: {
+        [evt: string]: CustomEvent<any>;
+    };
+    slots: {
+        default: {
+            trigger: (node: HTMLElement) => {
+                destroy(): void;
+            };
+        };
+        tooltip: {};
+    };
+};
+export type A2Props = typeof __propDef.props;
+export type A2Events = typeof __propDef.events;
+export type A2Slots = typeof __propDef.slots;
+export default class A2 extends SvelteComponentTyped<A2Props, A2Events, A2Slots> {
+}
+export {};
