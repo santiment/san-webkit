bced2141d5400fc2378fbca0b1a23168fa3298d8
DmitriVanGuard <dimavanguard@gmail.com>
2023-07-17 15:56:59 +0200

    feat(storybook): Icons story

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,76 +1,32 @@
-// const webpack = require('webpack')
-// const fs = require('fs')
-// const path = require('path')
-// const cssModules = require('svelte-preprocess-cssmodules')
-// const utils = require('../scripts/utils')
-// const { getSvgId } = require('../scripts/icons')
-//
-// const API_SERVER = process.env.BACKEND_URL
-//
-// function extractAttributeValue(string, anchor) {
-//   const start = string.indexOf(anchor) + anchor.length + 2
-//   const end = string.indexOf('"', start)
-//   return string.slice(start, end).toString()
-// }
-//
-// async function prepareIconsData(dir, filename) {
-//   const ICONS = []
-//   await utils.forFile([utils.SRC + dir + '/**/*.svg'], (entry) => {
-//     const file = fs.readFileSync(entry)
-//     const key = getSvgId(utils.SRC + dir + '/', entry)
-//     const width = extractAttributeValue(file, 'width')
-//     const height = extractAttributeValue(file, 'height')
-//     ICONS.push({ key, width, height })
-//   })
-//
-//   fs.writeFile(
-//     path.resolve(utils.ROOT, 'stories', filename + '.json'),
-//     JSON.stringify(ICONS),
-//     () => {},
-//   )
-// }
+const fs = require('fs')
+const path = require('path')
+const utils = require('../scripts/utils')
+const { prepareIcons, getSvgId } = require('../scripts/icons')
 
-// module.exports = {
-//   webpackFinal: async (config) => {
-//
-//     await Promise.all([
-//       prepareIconsData('/illus', 'illustrations'),
-//       prepareIconsData('/icons', 'icons'),
-//     ])
-//
-//     const svelteLoader = config.module.rules.find(
-//       (r) => r.loader && r.loader.includes('svelte-loader'),
-//     )
-//     svelteLoader.options.preprocess = [cssModules(), require('svelte-preprocess')({})]
-//
-//     config.resolve.alias['@'] = path.resolve(__dirname, '../src')
-//
-//     config.plugins.push(
-//       new webpack.DefinePlugin({
-//         'process.browser': true,
-//         'process.env.GQL_SERVER_URL': JSON.stringify(API_SERVER + '/graphql'),
-//         'process.env.IS_DEV_MODE': true,
-//         'process.env.MEDIA_PATH': JSON.stringify(''),
-//         'process.env.ICONS_PATH': JSON.stringify('/icons'),
-//         'process.env.IS_STAGE_BACKEND': true,
-//         'process.env.IS_PROD_BACKEND': false,
-//       }),
-//     )
-//
-//     return config
-//   },
-//   stories: [
-//     '../stories/**/*.stories.mdx',
-//     '../src/styles/*.css',
-//     '../stories/**/*.stories.@(js|jsx|ts|tsx|svelte)',
-//   ],
-//   addons: [
-//     '@storybook/addon-links',
-//     '@storybook/addon-essentials',
-//     '@storybook/addon-svelte-csf',
-//     './register.js',
-//   ],
-// }
+function extractAttributeValue(string, anchor) {
+  const start = string.indexOf(anchor) + anchor.length + 2
+  const end = string.indexOf('"', start)
+  return string.slice(start, end).toString()
+}
+
+async function prepareIconsData(dir, filename) {
+  const ICONS = []
+  await utils.forFile([utils.SRC + dir + '/**/*.svg'], (entry) => {
+    const file = fs.readFileSync(entry)
+    const key = getSvgId(utils.SRC + dir + '/', entry)
+    const width = extractAttributeValue(file, 'width')
+    const height = extractAttributeValue(file, 'height')
+    ICONS.push({ key, width, height })
+  })
+
+  fs.writeFile(
+    path.resolve(utils.ROOT, 'stories', filename + '.json'),
+    JSON.stringify(ICONS),
+    () => {},
+  )
+}
+
+utils.mkdir(utils.LIB)
 
 /** @type {import('@storybook/sveltekit').StorybookConfig} */
 const config = {
@@ -88,10 +44,17 @@ const config = {
     autodocs: 'tag',
   },
 
-  // staticDirs: ['../src'],
+  staticDirs: ['../lib'],
   // staticDirs: ['../src/icons/', '../src/illus/'],
 
   async viteFinal(config) {
+    await Promise.all([
+      utils.forFile(['src/{icons,illus}/**/*.svg'], utils.copyFile),
+      prepareIcons(),
+      prepareIconsData('/illus', 'illustrations'),
+      prepareIconsData('/icons', 'icons'),
+    ])
+
     config.server.fs.allow = ['../']
 
     Object.assign(config.define, {
@@ -100,6 +63,9 @@ const config = {
         const fetch = window.fetch
         return (...args) => (window.fetch.polyfill ? window.fetch : fetch)(...args)
       })())`,
+
+      'process.env.MEDIA_PATH': JSON.stringify(''),
+      'process.env.ICONS_PATH': JSON.stringify('/icons'),
     })
 
     config.optimizeDeps.exclude = ['san-webkit']
--- stories/DesignSystem/Icons/index.stories.ts
+++ stories/DesignSystem/Icons/index.stories.ts
@@ -3,7 +3,7 @@ import type { Meta, StoryObj } from '@storybook/svelte'
 import Component from './index.svelte'
 
 const meta = {
-  title: 'Design System/Buttons',
+  title: 'Design System/Icons',
   component: Component,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
   // tags: ['autodocs'],
@@ -13,9 +13,9 @@ const meta = {
 
     // docs: { story: { height: 100 } },
   },
-} satisfies Meta<Component>
+} satisfies Meta<Icons>
 type Story = StoryObj<typeof meta>
 
 export default meta
 
-export const Buttons: Story = {}
+export const Icons: Story = {}
