528b56eabccdeb9fe2ff6fc86c07c977a2e7919a
DmitriVanGuard <dimavanguard@gmail.com>
2023-07-23 15:57:29 +0200

    feat(storybook): API mock story

--- .storybook/Dialog.svelte
+++ .storybook/Dialog.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { onDestroy } from 'svelte'
   import { newDialogCtx } from '@/ui/Dialog/ctx'
   import { DialogLock } from '@/ui/Dialog/types'
 
@@ -9,6 +10,13 @@
   new Promise((resolve, reject) => (DialogPromise = { resolve, reject, locking: DialogLock.FREE }))
 
   const DialogCtx = newDialogCtx(DialogPromise)
+
+  onDestroy(() => {
+    document.body.style.width = ''
+    document.body.style.maxWidth = ''
+    document.body.style.overflowY = ''
+    document.body.style.touchAction = ''
+  })
 </script>
 
 <svelte:component this={render} i={0} {DialogPromise} {DialogCtx} />
