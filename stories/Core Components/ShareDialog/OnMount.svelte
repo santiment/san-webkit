61d31fc152fe59ad4c142a7c23b7942ab3d14d9d
Vlad Keliy <vekkele@gmail.com>
2023-07-24 12:33:40 +0300

    feature(stories migrate): Migrate Share Dialog stories to new architecture

---
+++ stories/Core Components/ShareDialog/OnMount.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+  import { onDestroy, onMount } from 'svelte'
+  import { dialogs } from '@/ui/Dialog'
+
+  export let run: () => void
+
+  onMount(run)
+  onDestroy(() => dialogs.hide(0))
+</script>
+
+<button class="btn-1" on:click={run}>Show dialog</button>
---
+++ stories/Core Components/ShareDialog/index.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { showShareDialog } from '@/ui/Share/index.svelte'
+  import OnMount from './OnMount.svelte'
+
+  export let props: Parameters<typeof showShareDialog>[0]
+</script>
+
+<OnMount run={() => showShareDialog(props)} />
