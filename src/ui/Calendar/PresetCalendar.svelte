3eb13b1a8a17d283c33cb2679a23f071c25531af
DmitriVanGuard <dimavanguard@gmail.com>
2023-07-26 23:04:23 +0200

    enhance(calendar): Forcing preset dates select

--- src/ui/Calendar/PresetCalendar.svelte
+++ src/ui/Calendar/PresetCalendar.svelte
@@ -8,31 +8,22 @@
   import Svg from '@/ui/Svg/svelte'
   import Calendar from './Calendar.svelte'
 
+  function Preset(title: string, target: 'Date' | 'Month' | 'FullYear', diff: number) {
+    const from = new Date()
+    from[`set${target}`](from[`get${target}`]() - diff)
+    return {
+      title,
+      presetDate: [from, new Date()],
+    }
+  }
+
   const PRESETS = [
-    {
-      title: 'Last day',
-      presetDate: [new Date().setDate(new Date().getDate() - 1), new Date()],
-    },
-    {
-      title: 'Last week',
-      presetDate: [new Date().setDate(new Date().getDate() - 7), new Date()],
-    },
-    {
-      title: 'Last month',
-      presetDate: [new Date().setMonth(new Date().getMonth() - 1), new Date()],
-    },
-    {
-      title: 'Last 3 month',
-      presetDate: [new Date().setMonth(new Date().getMonth() - 3), new Date()],
-    },
-    {
-      title: 'Last 6 month',
-      presetDate: [new Date().setMonth(new Date().getMonth() - 6), new Date()],
-    },
-    {
-      title: 'Last year',
-      presetDate: [new Date().setFullYear(new Date().getFullYear() - 1), new Date()],
-    },
+    Preset('Last day', 'Date', 1),
+    Preset('Last week', 'Date', 7),
+    Preset('Last month', 'Month', 1),
+    Preset('Last 3 months', 'Month', 3),
+    Preset('Last 6 months', 'Month', 6),
+    Preset('Last year', 'FullYear', 1),
     {
       title: 'All time',
       presetDate: [new Date(2009, 0, 1), new Date()],
@@ -47,6 +38,7 @@
 
   function onPresetClick(dates: [Date, Date]) {
     if (calendar) {
+      calendar.clear()
       calendar.selectDate(dates)
       calendar.setViewDate(dates[0])
     }
