ed6adf46c6967d3d3c289ac132cfa4e2df214ed7
Vlad Keliy <vekkele@gmail.com>
2023-07-28 16:34:30 +0300

    feature(stories migrate): Migrate Tooltip stories to new architecture

---
+++ stories/Core Components/Tooltip/TooltipStory.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import Tooltip from '@/ui/Tooltip/Tooltip.svelte'
+  import type { ComponentProps } from 'svelte'
+
+  export let position: ComponentProps<Tooltip>['position']
+</script>
+
+<Tooltip {position} let:trigger {...$$restProps}>
+  <div use:trigger>{position} position</div>
+
+  <div slot="tooltip" class="tooltip">
+    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+  </div>
+</Tooltip>
+
+<style>
+  .tooltip {
+    padding: 8px 12px;
+    max-width: 200px;
+  }
+</style>
---
+++ stories/Core Components/Tooltip/index.svelte
@@ -0,0 +1,40 @@
+<script lang="ts">
+  import type { ComponentProps } from 'svelte'
+  import type Tooltip from '@/ui/Tooltip'
+
+  import TooltipStory from './TooltipStory.svelte'
+
+  interface $$Props extends ComponentProps<Tooltip> {}
+</script>
+
+<main class="column hv-center">
+  <div class="row justify gap-xl">
+    <TooltipStory position="top-start" {...$$props} />
+    <TooltipStory position="top" {...$$props} />
+    <TooltipStory position="top-end" {...$$props} />
+  </div>
+
+  <div class="row justify gap-xl mrg-xxl mrg--t">
+    <TooltipStory position="right-start" {...$$props} />
+    <TooltipStory position="right" {...$$props} />
+    <TooltipStory position="right-end" {...$$props} />
+  </div>
+
+  <div class="row justify gap-xl mrg-xxl mrg--t">
+    <TooltipStory position="left-start" {...$$props} />
+    <TooltipStory position="left" {...$$props} />
+    <TooltipStory position="left-end" {...$$props} />
+  </div>
+
+  <div class="row justify gap-xl mrg-xxl mrg--t">
+    <TooltipStory position="bottom-start" {...$$props} />
+    <TooltipStory position="bottom" {...$$props} />
+    <TooltipStory position="bottom-end" {...$$props} />
+  </div>
+</main>
+
+<style>
+  main {
+    padding-top: 72px;
+  }
+</style>
