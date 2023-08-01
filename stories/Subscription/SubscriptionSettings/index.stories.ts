3bcd6c99d15324a662c4c61aab0d7dbc8251b6ed
Vlad Keliy <vekkele@gmail.com>
2023-08-01 11:46:57 +0300

    enhance(stories migrate): Add autodocs for Subscription Settings

--- stories/Subscription/SubscriptionSettings/index.stories.ts
+++ stories/Subscription/SubscriptionSettings/index.stories.ts
@@ -6,12 +6,12 @@ const meta = {
   // title: 'Design System/Icons',
   component: Component,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/react/writing-docs/autodocs
-  // tags: ['autodocs'],
+  tags: ['autodocs'],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/svelte/configure/story-layout
     layout: 'fullscreen',
 
-    // docs: { story: { height: 100 } },
+    docs: { story: { height: 800 } },
   },
 } satisfies Meta<Component>
 type Story = StoryObj<typeof meta>
