6aa852c38727c2c30dee804893120983595c6a2a
DmitriVanGuard <dimavanguard@gmail.com>
2023-08-17 10:55:48 +0300

    feat(dialogs): Typing show call. Ctx capture method

--- src/ui/Dialog/dialogs.ts
+++ src/ui/Dialog/dialogs.ts
@@ -1,4 +1,4 @@
-import type { ComponentType } from 'svelte'
+import { getAllContexts, type ComponentType, type SvelteComponent } from 'svelte'
 
 import { writable } from 'svelte/store'
 import { newDialogCtx } from './ctx'
@@ -11,6 +11,12 @@ export type DialogController = {
   reject: (reason?: any) => void
   locking: DialogLock
 }
+
+type SvelteProps<T> = T extends ComponentType<infer Component>
+  ? Component extends SvelteComponent<infer Props>
+    ? Props
+    : never
+  : never
 
 type DialogsStore = Array<{ Component: ComponentType; props: any }>
 
@@ -25,6 +31,14 @@ type Props = {
     locking: DialogLock
   }
 }
+
+type DialogComponentProps<T> = {
+  [K in keyof SvelteProps<T> as K extends 'DialogPromise' | 'i' | 'DialogCtx'
+    ? never
+    : K]: SvelteProps<T>[K]
+} & Partial<{
+  strict: boolean
+}>
 
 const DIALOGS = [] as DialogsStore
 const { subscribe, update } = writable<DialogsStore>(DIALOGS)
@@ -51,6 +65,53 @@ export const dialogs = {
   showOnce<T = unknown>(Component: ComponentType, props: Props = {}): undefined | Promise<T> {
     return dialogs.has(Component) ? undefined : dialogs.show(Component, props)
   },
+
+  __show<T extends ComponentType>(Component: T, props: DialogComponentProps<T> = {} as any) {
+    if (dialogs.has(Component)) return Promise.reject('Dialog is already opened')
+
+    const dialogProps = props as typeof props & {
+      DialogPromise?: SAN.DialogController
+      DialogCtx?: SAN.Dialog.Ctx
+      CTX?: Record<string, any>
+    }
+
+    const promise = new Promise<T>((resolve, reject) => {
+      dialogProps.DialogPromise = { resolve, reject, locking: DialogLock.FREE }
+    })
+
+    dialogProps.DialogCtx = newDialogCtx<T>(props.DialogPromise as SAN.Dialog.Promise)
+
+    update((dialogs) => {
+      dialogs.push({ Component, props: dialogProps })
+      return dialogs
+    })
+
+    return props.strict ? promise : promise.catch(pipeCatch)
+  },
+
+  /**
+   * Example:
+   * ```html
+   * <script context="module">
+   *   export const showPaymentDialog$ = () => dialogs.WithCtx(PaymentDialog)
+   * </script>
+   *
+   * <script>
+   *   const showPaymentDialog = showPaymentDialog$()
+   * ```
+   */
+  __WithCtx<T extends ComponentType, K extends (...args: any[]) => DialogComponentProps<T>>(
+    Component: T,
+    modifyProps: K = ((props: DialogComponentProps<T>) => props) as K,
+  ) {
+    const CTX = getAllContexts()
+
+    return (...args: Parameters<K>) => {
+      const props = Object.assign(modifyProps(...args) || {}, { CTX })
+      return dialogs.__show(Component, props)
+    }
+  },
+
   hide(index: number): void {
     update((dialogs) => {
       dialogs.splice(index, 1)
