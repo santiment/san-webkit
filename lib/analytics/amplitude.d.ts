4fc78bb349b8c2a9f6326b124199f9d887992c4e
DmitriVanGuard <dimavanguard@gmail.com>
2023-09-05 15:46:42 +0300

    Library release

--- lib/analytics/amplitude.d.ts
+++ lib/analytics/amplitude.d.ts
@@ -1,2 +1,3 @@
 export declare function initAmplitude(): void;
+export declare function setAmplitudeUserProperties(props: Record<string, any>): any;
 export declare function updateAmplitude(user_id?: number, name?: string | null, email?: string | null): void;
