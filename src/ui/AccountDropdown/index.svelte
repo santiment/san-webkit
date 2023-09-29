7940eadea5fd21a25a6200b7f34defeed839e9db
Vlad Keliy <vekkele@gmail.com>
2023-09-29 11:35:41 +0800

    enhance(account dropdown) Remove pricing link from AccountDropdown

--- src/ui/AccountDropdown/index.svelte
+++ src/ui/AccountDropdown/index.svelte
@@ -114,12 +114,6 @@
         >
       {/if}
 
-      <a
-        href="{SANBASE_ORIGIN}/pricing"
-        class="btn-ghost row justify v-center"
-        on:click={window.__onLinkClick}>Pricing</a
-      >
-
       <button
         class="btn-ghost row justify v-center"
         on:click={() => window.Intercom && window.Intercom('show')}
