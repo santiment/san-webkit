322262c1646e531053064e7a683338281edda191
Vlad Keliy <vekkele@gmail.com>
2023-10-12 10:05:46 +0300

    refactor(table) Refactor paged section layout and add className prop for paged section

--- src/ui/Table/Paged.svelte
+++ src/ui/Table/Paged.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
   import { noop } from '@/utils'
   import Svg from '@/ui/Svg/svelte'
-  import Tooltip from '@/ui/Tooltip/svelte'
+  import Tooltip from '@/ui/Tooltip'
   import Table from './index.svelte'
 
   let className = ''
   export { className as class }
+  export let pagedClassName = ''
   export let items
   export let pageSize = 25
   export let page = 0
@@ -54,20 +55,19 @@
   }
 </script>
 
-<div class={className}>
-  <Table {...$$restProps} items={pageItems} offset={pageOffset} {applySort}>
-    <slot />
-  </Table>
-</div>
+<Table {...$$restProps} class={className} items={pageItems} offset={pageOffset} {applySort}>
+  <slot />
+</Table>
 
-<div class="paged row v-center mrg-l mrg--t">
+<section class="paged row v-center gap-l mrg-l mrg--t single-line {pagedClassName}">
   {#if rows.length > 1}
-    <Tooltip on="click" position="top" bind:isOpened={isPageSizeOpened}>
-      <div slot="trigger" class="rows btn-2 btn--s mrg-s mrg--r row v-center justify">
-        {pageSize} rows <Svg id="arrow-down" w="8" h="5" />
-      </div>
+    <Tooltip on="click" position="top" bind:isOpened={isPageSizeOpened} let:trigger>
+      <button use:trigger class="rows-trigger btn-2 btn--s row v-center gap-m">
+        {pageSize} rows
+        <Svg id="arrow-down" w="8" h="5" />
+      </button>
 
-      <div slot="tooltip" class="pages column">
+      <tooltip slot="tooltip" class="pages column">
         {#each rows as row}
           <button
             class="btn-ghost"
@@ -75,52 +75,49 @@
             on:click={() => onPageSizeChange(row)}>{row} rows</button
           >
         {/each}
-      </div>
+      </tooltip>
     </Tooltip>
   {/if}
 
-  <span class="c-waterloo">Page</span>
+  <page-indicator class="row v-center gap-s c-waterloo">
+    <span>Page</span>
 
-  <input
-    type="number"
-    class="input txt-center mrg-l mrg--l mrg--r"
-    min="1"
-    value={page + 1}
-    on:change={onPageInput}
-    max={pagesAmount}
-  />
+    <input
+      type="number"
+      class="input txt-center"
+      min="1"
+      value={page + 1}
+      on:change={onPageInput}
+      max={pagesAmount}
+    />
 
-  <span class="c-waterloo"
-    >of {pagesAmount}
-    <span class="mrg-s mrg--l" />
-    ({items.length} rows total)</span
-  >
+    <span>of {pagesAmount}</span>
+  </page-indicator>
 
-  <button
-    class="btn-2 btn--s row hv-center mrg-a mrg--l"
-    class:disabled={page <= 0}
-    on:click={onPrevPage}
-  >
-    Prev
-    <Svg id="arrow-right" w="5" h="8" class="$style.left mrg-m mrg--l" />
-  </button>
-  <button
-    class="btn-2 btn--s row hv-center mrg-s mrg--l"
-    class:disabled={page >= maxPage}
-    on:click={onNextPage}
-  >
-    <Svg id="arrow-right" w="5" h="8" class="mrg-m mrg--r" />
-    Next
-  </button>
-</div>
+  <nav-buttons class="row mrg-a mrg--l">
+    <button class="btn-2 btn--s row hv-center" class:disabled={page <= 0} on:click={onPrevPage}>
+      Prev
+      <Svg id="arrow-right" w="5" h="8" class="$style.left mrg-m mrg--l" />
+    </button>
+
+    <button
+      class="btn-2 btn--s row hv-center mrg-s mrg--l"
+      class:disabled={page >= maxPage}
+      on:click={onNextPage}
+    >
+      <Svg id="arrow-right" w="5" h="8" class="mrg-m mrg--r" />
+      Next
+    </button>
+  </nav-buttons>
+</section>
 
 <style lang="scss">
   .left {
     transform: rotate(180deg);
   }
 
-  .rows {
-    min-width: 97px;
+  .rows-trigger {
+    --h-padding: 13px;
   }
 
   .paged {
@@ -137,12 +134,19 @@
 
   input {
     width: 45px;
+    appearance: textfield;
     -moz-appearance: textfield;
 
     &::-webkit-outer-spin-button,
     &::-webkit-inner-spin-button {
       -webkit-appearance: none;
       margin: 0;
+    }
+  }
+
+  @include dac(phone-xs) {
+    .rows-trigger {
+      display: none;
     }
   }
 </style>
