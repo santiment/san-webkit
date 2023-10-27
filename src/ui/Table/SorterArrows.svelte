d10a0b2fac39ed117c92cd2ca7a9160d59273723
DmitriVanGuard <dimavanguard@gmail.com>
2023-10-27 15:32:03 +0200

    enhance(table): Default sorting - descending - on sort change

--- src/ui/Table/SorterArrows.svelte
+++ src/ui/Table/SorterArrows.svelte
@@ -4,19 +4,14 @@
   export let isAscending = false
 </script>
 
-<Svg
-  illus
-  id="sorted"
-  w="6"
-  h="8"
-  class="$style.sorter mrg-xs mrg--l {isAscending ? '$style.asc' : ''}"
-/>
+<Svg illus id="sorted" w="6" h="8" class="mrg-xs mrg--l {isAscending ? '$style.asc' : ''}" />
 
 <style>
-  .sorter {
+  Svg {
     vertical-align: initial;
     margin-right: -14px;
   }
+
   .asc {
     --sorter-up: var(--waterloo);
     --sorter-down: var(--mystic);
