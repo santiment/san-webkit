d69aae10dc84541f5b8d45cba500790212afd938
DmitriVanGuard <dimavanguard@gmail.com>
2023-11-01 12:34:16 +0100

    Library release

--- lib/utils/events.d.ts
+++ lib/utils/events.d.ts
@@ -1,1 +1,3 @@
+/// <reference types="svelte" />
 export declare function newGlobalShortcut(shortcut: string, clb: () => any, disableInputs?: boolean): () => void;
+export declare function GlobalShortcut$(shortcut: string, clb: () => any, disableInputs?: boolean): import("svelte/store").Readable<null>;
