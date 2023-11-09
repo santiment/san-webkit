2c4b3fff67907a54a16bafbff77f84d7f8fcd4e2
Vlad Keliy <vekkele@gmail.com>
2023-11-09 14:26:53 +0300

    feature(account dropdown) Add 'back to classic version' button to dropdown story

--- stories/Core Components/AccountDropdown/index.svelte
+++ stories/Core Components/AccountDropdown/index.svelte
@@ -1,15 +1,22 @@
 <script lang="ts">
+  import { UI$$ } from '@/stores/ui'
   import AccountDropdown from '@/ui/AccountDropdown/index.svelte'
 
   export let currentUser: SAN.Author | null
 
+  const { ui$ } = UI$$({ isLiteVersion: true })
+
   function onLogoutClick() {
     console.log('LOG OUT')
+  }
+
+  function onOldVersionClick() {
+    $ui$.isLiteVersion = false
   }
 </script>
 
 <section class="row hv-center">
-  <AccountDropdown {currentUser} {onLogoutClick} />
+  <AccountDropdown {currentUser} {onLogoutClick} {onOldVersionClick} />
 </section>
 
 <style>
