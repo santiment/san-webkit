ec47e4a5bc2d60b3f94d3f2cd7a45142c60c1284
Vlad Keliy <vekkele@gmail.com>
2023-12-06 11:41:49 +0300

    fix(table) Fix storybook story for page table with fetching

--- stories/Core Components/Table/PagedWithFetching.svelte
+++ stories/Core Components/Table/PagedWithFetching.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+  import { onMount } from 'svelte'
   import Table from '@/ui/Table/Paged.svelte'
   import { Item, columns, generateItems } from './mock'
 
+  let pageSize = 5
   let items: Item[] = []
 
   async function onPageChange(page: number, pageSize: number) {
@@ -16,8 +18,12 @@
       }, 300)
     })
   }
+
+  onMount(() => {
+    onPageChange(0, 5)
+  })
 </script>
 
 <section>
-  <Table {items} {columns} totalItems={20} pageSize={5} rows={[5, 10, 20]} {onPageChange} />
+  <Table {items} {columns} totalItems={20} bind:pageSize rows={[5, 10, 20]} {onPageChange} />
 </section>
