4f943e83929ab8874bb1a7f4a7060131cd20ba9d
Vlad Keliy <vekkele@gmail.com>
2023-12-11 13:09:15 +0300

    feature(mini chart) Add multi chart

---
+++ src/ui/MiniChart/SvgTooltip.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  export let valueFormatted = ''
+</script>
+
+<tooltip class="c-fiord">
+  {#if $$slots.value}
+    <slot name="value" />
+  {:else if valueFormatted}
+    {valueFormatted}
+  {/if}
+</tooltip>
+
+<style>
+  tooltip {
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: color-mix(in srgb, var(--white) 80%, transparent);
+    border-radius: 4px;
+    padding: 2px 4px;
+    pointer-events: none;
+  }
+</style>
