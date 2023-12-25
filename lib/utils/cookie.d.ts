914a2a23483331d5bc9808ad9e0809600e004590
DmitriVanGuard <dimavanguard@gmail.com>
2023-12-25 22:18:24 +0300

    Library release

--- lib/utils/cookie.d.ts
+++ lib/utils/cookie.d.ts
@@ -1,2 +1,3 @@
 export declare function getCookie(name: string, predicate?: (keyValue: string) => boolean): string | undefined;
 export declare function setCookie(key: string, value: number | boolean | string): void;
+export declare function deleteCookie(key: string): void;
