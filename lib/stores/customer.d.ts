92d847e09acaa18baba6d387189d402fda321fb1
DmitriVanGuard <dimavanguard@gmail.com>
2024-01-16 12:40:00 +0300

    Library release

--- lib/stores/customer.d.ts
+++ lib/stores/customer.d.ts
@@ -16,7 +16,8 @@ export type CustomerType = {
     isCanceled: boolean;
 };
 export declare const DEFAULT: CustomerType;
-export declare const CUSTOMER_QUERY = "{\n  currentUser {\n    isEligibleForTrial:isEligibleForSanbaseTrial\n    subscriptions {\n      id\n      status\n      trialEnd\n      cancelAtPeriodEnd\n      currentPeriodEnd\n      plan {\n        id\n        name\n        amount\n        interval\n        product { id }\n      }\n    }\n  }\n  annualDiscount:checkAnnualDiscountEligibility {\n    isEligible\n    discount {\n      percentOff\n      expireAt\n    }\n  }\n}";
+export declare const CUSTOMER_QUERY = "{\n  currentUser {\n    isEligibleForTrial:isEligibleForSanbaseTrial\n    subscriptions {\n      id\n      status\n      trialEnd\n      cancelAtPeriodEnd\n      currentPeriodEnd\n      plan {\n        id\n        name\n        amount\n        interval\n        product { id }\n      }\n    }\n  }\n}";
+export declare const ANNUAL_DISCOUNT_QUERY = "{\n  annualDiscount:checkAnnualDiscountEligibility {\n    isEligible\n    discount {\n      percentOff\n      expireAt\n    }\n  }\n}";
 export declare const queryCustomer: (requestEvent?: import("./../api").RequestEvent | undefined) => Promise<CustomerType>;
 export declare function Customer$$(defaultValue?: CustomerType): {
     customer$: {
