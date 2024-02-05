fa1f2a75c73a6e449038c6da4dc8db4059c4390a
Vlad Keliy <vekkele@gmail.com>
2024-02-05 14:41:10 +0300

    feature(auth) Add optional redirect path parameter to redirect to it after oauth login

--- src/ui/LoginPrompt/Google.svelte
+++ src/ui/LoginPrompt/Google.svelte
@@ -1,14 +1,16 @@
-<script>
+<script lang="ts">
   import { LoginType, trackAuthStart, trackLoginStart } from '@/analytics/events/general'
   import { trackSignupStart } from '@/analytics/events/onboarding'
   import { getOAuthLink } from '@/utils/auth'
   import Option from './Option.svelte'
+
+  export let from = ''
 </script>
 
 <Option
   {...$$props}
   title="Google"
-  href={getOAuthLink('google')}
+  href={getOAuthLink('google', from)}
   icon="google"
   on:click={() => {
     trackAuthStart(LoginType.GOOGLE)
