aa8847e65de7e7b3f39db6fec3d6681160e445b5
Vlad Keliy <vekkele@gmail.com>
2024-05-28 11:53:26 +0300

    feature(calendar) Adjust basic calendar styles

--- src/lib/ui/Calendar/Calendar.svelte
+++ src/lib/ui/Calendar/Calendar.svelte
@@ -17,23 +17,24 @@
     <RangeCalendar.PrevButton
       class="rounded-9px bg-background-alt hover:bg-muted active:scale-98 inline-flex size-8 items-center justify-center"
     >
-      <Svg id="arrow-left-big"></Svg>
+      <Svg id="arrow-left-big" />
     </RangeCalendar.PrevButton>
     <RangeCalendar.Heading class="text-[15px] font-medium" />
     <RangeCalendar.NextButton
       class="rounded-9px bg-background-alt hover:bg-muted active:scale-98 inline-flex size-8 items-center justify-center"
     >
-      <Svg id="arrow-right-big"></Svg>
+      <Svg id="arrow-right-big" />
     </RangeCalendar.NextButton>
   </RangeCalendar.Header>
-  <div class="flex flex-col space-y-4 pt-4 sm:flex-row sm:space-x-4 sm:space-y-0">
+
+  <div class="flex flex-col space-y-4 px-3 pb-3 pt-4 sm:flex-row sm:space-x-4 sm:space-y-0">
     {#each months as month}
       <RangeCalendar.Grid class="w-full border-collapse select-none space-y-1">
         <RangeCalendar.GridHead>
           <RangeCalendar.GridRow class="mb-1 flex w-full justify-between">
             {#each weekdays as day}
               <RangeCalendar.HeadCell
-                class="text-muted-foreground w-10 rounded-md text-xs !font-normal"
+                class="text-muted-foreground w-8 rounded-md text-xs !font-normal uppercase text-waterloo"
               >
                 <div>{day.slice(0, 2)}</div>
               </RangeCalendar.HeadCell>
@@ -46,25 +47,23 @@
               {#each weekDates as date}
                 <RangeCalendar.Cell
                   {date}
-                  class="relative m-0 size-8 !p-0 text-center text-sm focus-within:z-20"
+                  class="relative m-0 size-8 w-8 !p-0 text-center text-sm focus-within:z-20"
                 >
                   <RangeCalendar.Day
                     {date}
                     month={month.value}
                     class={cn(
-                      'size-8 rounded hover:bg-athens hover:text-green',
+                      'size-full rounded hover:bg-athens hover:text-green',
+                      'data-[today]:[&:not([data-highlighted])]:[&:not([data-selected])]:[&:not(:hover)]:text-blue',
                       'data-[selected]:bg-green-light-1 data-[selected]:text-green',
                       'data-[unavailable]:text-mystic',
                       'data-[disabled]:text-mystic',
                       'data-[highlighted]:rounded-none data-[highlighted]:bg-green-light-1',
                       'data-[selected]:[&:not([data-selection-start])]:[&:not([data-selection-end])]:focus-visible:border-foreground group relative inline-flex items-center justify-center overflow-visible whitespace-nowrap border border-transparent bg-transparent p-0 text-sm font-normal data-[disabled]:pointer-events-none data-[outside-month]:pointer-events-none  data-[selected]:font-medium data-[selection-end]:font-medium data-[selection-start]:font-medium  data-[unavailable]:line-through data-[selection-start]:focus-visible:ring-2 data-[selection-start]:focus-visible:!ring-offset-2 data-[selected]:[&:not([data-selection-start])]:[&:not([data-selection-end])]:rounded-none data-[selected]:[&:not([data-selection-start])]:[&:not([data-selection-end])]:focus-visible:!ring-0 data-[selected]:[&:not([data-selection-start])]:[&:not([data-selection-end])]:focus-visible:!ring-offset-0',
-                      'data-[selection-start]:bg-green-light-2 data-[selection-start]:rounded-e-none data-[selection-start]:rounded-s',
-                      'data-[selection-end]:bg-green-light-2 data-[selection-end]:rounded-e data-[selection-end]:rounded-s-none',
+                      'data-[selection-start]:rounded-e-none data-[selection-start]:rounded-s data-[selection-start]:bg-green-light-2',
+                      'data-[selection-end]:rounded-e data-[selection-end]:rounded-s-none data-[selection-end]:bg-green-light-2',
                     )}
                   >
-                    <div
-                      class="bg-foreground absolute top-[5px] hidden size-1 rounded-full group-data-[today]:block group-data-[selected]:bg-red"
-                    />
                     {date.day}
                   </RangeCalendar.Day>
                 </RangeCalendar.Cell>
