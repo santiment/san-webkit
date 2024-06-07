aeecdaf53cee85a99597986ddaef84968a9dd550
Vlad Keliy <vekkele@gmail.com>
2024-06-07 11:59:25 +0300

    fix(css) Fix componentStyleSelector preprocessor

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,10 @@
 import adapter from '@sveltejs/adapter-auto'
-import { sequence } from '@sveltejs/kit/hooks'
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { componentStyleSelector } from './plugins/svelte.js'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  preprocess: sequence([vitePreprocess(), componentStyleSelector()]),
+  preprocess: [vitePreprocess(), componentStyleSelector()],
 
   kit: {
     adapter: adapter(),
