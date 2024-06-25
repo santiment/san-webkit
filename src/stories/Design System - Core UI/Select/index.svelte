cd69b4ac43c07d17f0c983f929cb141abb4b29bb
Vlad Keliy <vekkele@gmail.com>
2024-06-25 14:36:53 +0300

    feature(select) Add simple select component

---
+++ src/stories/Design System - Core UI/Select/index.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  import type { Selected } from '$ui/core/Select/index.js'
+
+  import Select from '$ui/core/Select/index.js'
+
+  const items: Selected<string>[] = [
+    { value: 'apples', label: 'Apples' },
+    { value: 'oranges', label: 'Oranges' },
+    { value: 'bananas', label: 'Bananas' },
+  ]
+
+  let selected = $state(items[0])
+</script>
+
+<div class="flex flex-col justify-center gap-4 p-6">
+  <div class="flex flex-row items-center gap-2">
+    <Select {items} bind:selected />
+  </div>
+</div>
