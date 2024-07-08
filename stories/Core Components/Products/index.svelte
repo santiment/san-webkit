fe50ffc805a74a36eef4804c62ab257acead0c52
Vlad Keliy <vekkele@gmail.com>
2024-07-08 09:19:30 +0300

    fix(products) Fix layout in column and compact variations

--- stories/Core Components/Products/index.svelte
+++ stories/Core Components/Products/index.svelte
@@ -2,4 +2,39 @@
   import Products from '@/ui/Products/Products.svelte'
 </script>
 
-<Products />
+<main class="column gap-xl">
+  <div>
+    <h2 class="h5">Regular</h2>
+    <div class="border">
+      <Products />
+    </div>
+  </div>
+
+  <div>
+    <h2 class="h5">Column</h2>
+    <div class="border">
+      <Products isColumn />
+    </div>
+  </div>
+
+  <div>
+    <h2 class="h5">Compact</h2>
+    <div class="border">
+      <Products isCompact />
+    </div>
+  </div>
+
+  <div>
+    <h2 class="h5">Column + Compact</h2>
+    <div class="border">
+      <Products isCompact isColumn />
+    </div>
+  </div>
+</main>
+
+<style>
+  main {
+    align-items: flex-start;
+    padding: 40px 20px;
+  }
+</style>
