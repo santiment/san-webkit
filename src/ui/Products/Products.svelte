b68e76d61eb15d20210f83005a7b813aa34d8362
Vlad Keliy <vekkele@gmail.com>
2024-07-18 13:44:41 +0300

    fix(products) Fix layout in column and compact variations

--- src/ui/Products/Products.svelte
+++ src/ui/Products/Products.svelte
@@ -10,14 +10,17 @@
   export let active: string | undefined = undefined
 </script>
 
-<div class="products row body-3 {className}" class:compact={isCompact} class:column={isColumn}>
-  {#if isColumn}
-    <section class="more body-2 c-waterloo mrg-xl mrg--t">
-      <!-- svelte-ignore security-anchor-rel-noreferrer -->
-      To learn more about Santiment's products, team, or available jobs, visit
-      <a class="link-pointer" target="_blank" href="https://santiment.net/">santiment.net</a>
-    </section>
-  {/if}
+<div
+  class="products row body-3 gap-xl {className}"
+  class:compact={isCompact}
+  class:column={isColumn}
+>
+  <section class="business column">
+    <h3>SAN Business</h3>
+    {#each business as product}
+      <Product {...product} active={active === product.id} {isCompact} />
+    {/each}
+  </section>
 
   <section class="chain column">
     <h3>SAN Chain</h3>
@@ -26,12 +29,13 @@
     {/each}
   </section>
 
-  <section class="business column mrg-xl mrg--l">
-    <h3>SAN Business</h3>
-    {#each business as product}
-      <Product {...product} active={active === product.id} {isCompact} />
-    {/each}
-  </section>
+  {#if isColumn}
+    <section class="more body-2 c-waterloo">
+      <!-- svelte-ignore security-anchor-rel-noreferrer -->
+      To learn more about Santiment's products, team, or available jobs, visit
+      <a class="link-pointer" target="_blank" href="https://santiment.net/">santiment.net</a>
+    </section>
+  {/if}
 </div>
 
 <style lang="scss">
@@ -52,10 +56,9 @@
   }
 
   .products.column {
-    flex-direction: column-reverse;
+    flex-direction: column;
 
     .business {
-      margin: 0 0 24px;
       padding: 0 0 24px;
       border-bottom: 1px solid var(--porcelain);
     }
