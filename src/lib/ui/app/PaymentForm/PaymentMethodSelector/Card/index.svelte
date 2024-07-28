5183fe45b40161832bbe54c98b1465615dcd16ca
DmitriVanGuard <dimavanguard@gmail.com>
2024-07-28 23:26:39 +0200

    feat(payment): Using stripe address input

--- src/lib/ui/app/PaymentForm/PaymentMethodSelector/Card/index.svelte
+++ src/lib/ui/app/PaymentForm/PaymentMethodSelector/Card/index.svelte
@@ -27,25 +27,78 @@
 
     const stripeCard = stripe.$.elements().create('card', SETTINGS)
     stripeCard?.mount('#card-element')
+
+    // const elements = stripe.$.elements({
+    //   clientSecret: 'seti_1Phe0qCA0hGU8IEVhFY3DjL8_secret_QYlXrQXQuVhn3BbugcuKK0bQM7mEZBD',
+    // })
+    // const addressElement = elements.create('address', { mode: 'billing' })
+    // addressElement.mount('#address-element')
+
+    const getCssColor = (color: string) =>
+      getComputedStyle(document.documentElement).getPropertyValue(`--${color}`)
+
+    // Create and mount the Address Element in shipping mode
+    const addressElement = stripe.$.elements({
+      clientSecret: '',
+      fonts: [
+        {
+          family: 'Proxima Nova',
+          src: 'url(/src/lib/fonts/ProximaNova-Regular.woff2)',
+          weight: '400',
+        },
+        {
+          family: 'Proxima Nova',
+          src: 'url(/src/lib/fonts/ProximaNova-Semibold.woff2)',
+          weight: '600',
+        },
+      ],
+      appearance: {
+        rules: {
+          '.Label': {
+            fontSize: '12px',
+            lineHeight: '16px',
+            fontWeight: '600',
+            marginBottom: '3px',
+            color: getCssColor('waterloo'),
+          },
+          '.Input': {
+            boxShadow: 'none',
+            padding: '11px 16px',
+            height: '40px',
+            fontSize: '14px',
+            color: getCssColor('black'),
+            transition: 'none',
+          },
+          '.Input::placeholder': {
+            color: getCssColor('casper'),
+          },
+          '.Input:hover': {
+            borderColor: getCssColor('green'),
+          },
+
+          '.Input:focus': {
+            boxShadow: 'none',
+            borderColor: getCssColor('green'),
+          },
+        },
+        variables: {
+          fontFamily: 'Proxima Nova',
+          borderRadius: '4px',
+          colorBackground: getCssColor('white'),
+          gridRowSpacing: '16px',
+        },
+      },
+    }).create('address', {
+      mode: 'billing',
+    })
+    addressElement.mount('#address-element')
   })
 </script>
 
-<div class="grid grid-cols-2 gap-4">
-  <LabelInput label="Card number" class="col-span-2">
+<div class="grid gap-4">
+  <LabelInput label="Card number" class="">
     <div id="card-element" class="h-10 rounded-md border px-4 py-2.5 hover:border-green"></div>
   </LabelInput>
 
-  <LabelInput label="First name" placeholder="Thomas"></LabelInput>
-
-  <LabelInput label="Last name" placeholder="Johnson"></LabelInput>
-
-  <LabelInput name="address_country" label="Country" placeholder="United States"></LabelInput>
-
-  <LabelInput name="address_state" label="State / Region" placeholder="e.g. California"
-  ></LabelInput>
-
-  <LabelInput name="address_city" label="City" placeholder="e.g. Sacramento"></LabelInput>
-
-  <LabelInput name="address_line1" label="Street Address" placeholder="e.g. 1483 Pearl Street"
-  ></LabelInput>
+  <div id="address-element"></div>
 </div>
