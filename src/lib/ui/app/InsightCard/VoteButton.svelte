9b03ca4aecb457f765abc1da707a98e5a9eb7014
serafim-san <serafim.g@santiment.net>
2024-08-08 19:14:06 +0300

    feat(insight card) added base like btn

---
+++ src/lib/ui/app/InsightCard/VoteButton.svelte
@@ -0,0 +1,24 @@
+<script>
+  import LikeBtn from '$lib/ui/app/LikeButton/LikeButton.svelte'
+
+  const { class: className = '', insight, source } = $props();
+  let { id, votes } = $derived(insight);
+ 
+  function onVoted() {
+    console.log('vote!')
+  }
+
+  // TODO:
+  // 1. Add mutation
+  // 2. If that is not current user than disable
+  // 3. Move analytics back
+</script>
+
+<LikeBtn
+  {id}
+  {source}
+  {onVoted}
+  type="insightId"
+  {votes}
+  class={className}
+/>
---
+++ src/lib/ui/app/LikeButton/LikeButton.svelte
@@ -0,0 +1,154 @@
+<script lang="ts">
+  import { onDestroy } from 'svelte'
+  import Rocket from './Rocket.svelte'
+  import Moon from './Moon.svelte'
+
+  let {
+    class: className = '',
+    totalVotes = 0,
+    userVotes = 0,
+    disabled = false,
+    onVote = () => {},
+    hasBorder = true,
+    maxVotesPerUser = 20,
+    voteInterval = 370 } = $props();
+
+  let rocketNode: HTMLElement
+  let moonNode: HTMLElement
+  let mooned = false
+  let timer: number
+  let votingInterval: number
+
+  function startVote(event: any) {
+    if (disabled) return
+
+    // Checking right button mouse click
+    if (event.button === 2) {
+      return
+    }
+
+    clearTimeout(timer)
+    clearInterval(votingInterval)
+
+    vote()
+    votingInterval = window.setInterval(vote, voteInterval)
+    window.addEventListener(event.type === 'mousedown' ? 'mouseup' : 'touchend', stopVote, {
+      once: true,
+    })
+  }
+
+  function vote() {
+    if (userVotes < maxVotesPerUser) {
+      userVotes += 1
+      totalVotes += 1
+      onVote()
+    }
+
+    resetAnimation(rocketNode)
+    resetAnimation(rocketNode.lastChild as HTMLElement)
+    if (mooned && moonNode) resetAnimation(moonNode)
+    mooned = true
+  }
+
+  function stopVote() {
+    clearInterval(votingInterval)
+    timer = window.setTimeout(() => (mooned = false), 1000)
+  }
+
+  function resetAnimation(node: HTMLElement) {
+    node.style.animation = 'none'
+    node.offsetWidth
+    node.style.animation = ''
+  }
+
+  onDestroy(() => {
+    clearTimeout(timer)
+    clearInterval(votingInterval)
+  })
+</script>
+
+<button
+  class="btn row v-center txt-m {className}"
+  class:voted={userVotes > 0}
+  class:disabled
+  class:border={hasBorder}
+  on:mousedown|preventDefault={startVote}
+  on:touchstart|preventDefault={startVote}
+>
+  <Moon bind:moonNode {mooned} votes={userVotes} />
+
+  <Rocket bind:rocketNode />
+
+  <span style="--digits:{totalVotes.toString().length}">{totalVotes}</span>
+</button>
+
+<style lang="postcss">
+  .btn {
+    padding: 5px 10px 5px 11px;
+    animation-timing-function: cubic-bezier(0.7, -0.53, 0.14, 3.52);
+    position: relative;
+    border-radius: 100px;
+    --color-hover: var(--green);
+  }
+  .btn:not(.disabled) {
+    --color: var(--waterloo);
+    --fill-hover: var(--waterloo);
+  }
+
+  .btn:hover :global(.rocket) {
+    animation: shake 1s infinite;
+  }
+  .btn:hover span {
+    color: var(--green);
+  }
+
+  .btn.voted {
+    background: var(--green-light-1);
+    --border: var(--green);
+    --color: var(--green);
+    --color-hover: var(--green-hover);
+    --border-hover: var(--green-hover);
+    --fill-hover: var(--green-hover);
+  }
+
+  span {
+    width: calc(var(--digits) * 1ch);
+    text-align: left;
+  }
+
+  @keyframes shake {
+    0% {
+      transform: translate(0, -2%);
+    }
+    10% {
+      transform: translate(1%, -1%);
+    }
+    20% {
+      transform: translate(2%, 0);
+    }
+    30% {
+      transform: translate(-4%, 1%);
+    }
+    40% {
+      transform: translate(4%, -1%);
+    }
+    50% {
+      transform: translate(-4%, 0);
+    }
+    60% {
+      transform: translate(4%, 0);
+    }
+    70% {
+      transform: translate(-4%, 0);
+    }
+    80% {
+      transform: translate(2%, -2%);
+    }
+    90% {
+      transform: translate(-1%, -2%);
+    }
+    100% {
+      transform: translate(0, -3%);
+    }
+  }
+</style>
