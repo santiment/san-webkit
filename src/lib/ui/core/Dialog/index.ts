72aa7213c2fafeccee1f721bae119702addaa933
Vlad Keliy <vekkele@gmail.com>
2024-08-27 11:48:45 +0300

    [RELEASE] 31e5feab

--- src/lib/ui/core/Dialog/index.ts
+++
@@ -1,4 +0,0 @@
-export { default } from './Dialog.svelte'
-export { dialogs$, getDialogControllerCtx } from './dialogs.js'
-
-export type { TDialogProps, TDialogReject, TDialogResolve } from './dialogs.js'
