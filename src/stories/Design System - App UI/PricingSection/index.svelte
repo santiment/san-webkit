d1436784b1182a87224a49dc20394430cf7b192e
Vlad Keliy <vekkele@gmail.com>
2024-09-03 11:58:28 +0300

    feature(pricing) Add PricingSection story

---
+++ src/stories/Design System - App UI/PricingSection/index.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import type { ComponentProps } from 'svelte'
+
+  import { showPaymentDialog$ } from '$ui/app/PaymentForm/Dialog.svelte'
+  import PricingSection from '$ui/app/SubscriptionPlan/PricingSection.svelte'
+  import { useSubscriptionPlanButtonCtx } from '$ui/app/SubscriptionPlan/ctx'
+
+  const props: ComponentProps<PricingSection> = $props()
+
+  const showPaymentDialog = showPaymentDialog$()
+
+  useSubscriptionPlanButtonCtx({
+    onPlanButtonClick: (plan) => showPaymentDialog({ defaultPlan: plan }),
+  })
+</script>
+
+<PricingSection {...props}></PricingSection>
