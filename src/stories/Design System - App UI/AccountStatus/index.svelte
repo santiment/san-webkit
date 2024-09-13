d0006e047dc43fef9554be211e4f7a6cd17331d8
Vlad Keliy <vekkele@gmail.com>
2024-09-13 12:02:04 +0300

    feature(pricing) Add AccountStatus component

---
+++ src/stories/Design System - App UI/AccountStatus/index.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+  import AccountStatus from '$ui/app/AccountStatus.svelte'
+</script>
+
+<main class="flex min-h-[inherit] items-center justify-center">
+  <AccountStatus />
+</main>
