13501417fc031dabf004aa612a197923d336fc2f
DmitriVanGuard <dimavanguard@gmail.com>
2024-09-22 17:51:12 +0200

    fix(analytics): Allowing overwrite of source_ fields in trackEvent

--- src/lib/analytics/events/index.ts
+++ src/lib/analytics/events/index.ts
@@ -16,11 +16,11 @@ export const trackEvent = (
   track.event(event, {
     category: 'General',
 
-    ...props,
-
     source_url: window.location.origin + window.location.pathname,
     source_url_type: window.getPageType?.(window.location.pathname),
     source_search_params: window.location.search,
+
+    ...props,
 
     release_version: process.env.VERSION || process.env.GIT_HEAD,
   })
