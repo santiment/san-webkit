3df0dd5c4b9fe90ce24337c98a231536c1c076e9
Vlad Keliy <vekkele@gmail.com>
2024-10-09 13:55:36 +0300

    feature(login form) Add Section and Option components

---
+++ src/lib/ui/app/LoginForm/Option.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+  import type { MouseEventHandler } from 'svelte/elements'
+
+  import Button from '$ui/core/Button/Button.svelte'
+
+  type Props = {
+    class?: string
+    title: string
+    href: string
+    icon: string
+    loading?: boolean
+    isSignUp?: boolean
+    onclick: MouseEventHandler<HTMLButtonElement>
+  }
+
+  const {
+    class: className,
+    title,
+    href,
+    icon,
+    loading = false,
+    isSignUp = false,
+    onclick,
+  }: Props = $props()
+</script>
+
+<Button variant="border" size="lg" class={className} {loading} {href} {onclick}>
+  {#if icon}
+    <img src="/webkit/icons/{icon}.svg" alt={icon} />
+  {/if}
+
+  {isSignUp ? 'Sign up' : 'Log in'} with {title}
+</Button>
