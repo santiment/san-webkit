c922049857b8baa68870b448b9cac299413b0f50
DmitriVanGuard <dimavanguard@gmail.com>
2025-02-18 19:58:17 +0100

    refactor(analytics): SvelteKit hook and client init refactored

--- src/lib/sveltekit/hooks/posthog.ts
+++ src/lib/sveltekit/hooks/posthog.ts
@@ -1,43 +1,34 @@
 import { error, json, text, type Handle } from '@sveltejs/kit'
 
-const POSTHOG_URL = 'https://eu.i.posthog.com'
-const PROXY_ROUTE = '/api/track/posthog'
+import { POSTHOG_URL, PROXY_ROUTE } from '$lib/analytics/posthog/index.js'
 
 export const posthogTrackHandle: Handle = async ({ event, resolve }) => {
-  if (event.url.pathname.startsWith(PROXY_ROUTE)) {
-    const route = event.url.pathname.replace(PROXY_ROUTE, '')
-    const targetUrl = `${POSTHOG_URL}/${route}${event.url.search}`
-
-    const clonedRequest = event.request.clone()
-
-    try {
-      const options: RequestInit = {
-        method: clonedRequest.method,
-        headers: clonedRequest.headers,
-      }
-
-      if (clonedRequest.method !== 'GET' && clonedRequest.method !== 'HEAD') {
-        options.body = await clonedRequest.text()
-
-        const headers = new Headers(clonedRequest.headers)
-        headers.delete('content-length')
-        options.headers = headers
-      }
-
-      const res = await fetch(targetUrl, options)
-
-      if (clonedRequest.method === 'GET' || clonedRequest.method === 'HEAD') {
-        const responseText = await res.text()
-        return text(responseText, { status: res.status })
-      } else {
-        const responseJson = await res.json()
-        return json(responseJson, { status: res.status })
-      }
-    } catch (e) {
-      console.error(e)
-      return error(500, 'Internal server error')
-    }
+  if (!event.url.pathname.startsWith(PROXY_ROUTE)) {
+    return resolve(event)
   }
 
-  return resolve(event)
+  const route = event.url.pathname.replace(PROXY_ROUTE, '')
+  const targetUrl = `${POSTHOG_URL}/${route}${event.url.search}`
+
+  const { method, headers } = event.request
+
+  let duplex = undefined
+
+  if (method === 'POST') {
+    duplex = 'half'
+    headers.delete('content-length')
+  }
+
+  try {
+    // @ts-expect-error duplex is not present in default Node.js types
+    const response = await fetch(targetUrl, { method, headers, duplex, body: event.request.body })
+    const isJson = (response.headers.get('content-type') ?? '').includes('application/json')
+
+    const data = await (isJson ? response.json() : response.text())
+
+    return (isJson ? json : text)(data, { status: response.status })
+  } catch (e) {
+    console.error(e)
+    return error(500, 'Internal server error')
+  }
 }
