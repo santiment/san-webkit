90cf3bcfe3ff79609b1bc656f60b3e6efa4da2d0
DmitriVanGuard <dimavanguard@gmail.com>
2025-03-03 21:30:54 +0100

    fix(chart): Using Path2D only in BROWSER env

--- src/lib/ui/app/Chart/SpikeExplanations/flow.svelte.ts
+++ src/lib/ui/app/Chart/SpikeExplanations/flow.svelte.ts
@@ -1,3 +1,4 @@
+import { BROWSER } from 'esm-env'
 import {
   drawCircle,
   drawSvgIcon,
@@ -9,9 +10,10 @@ import {
 
 export const AI_ICON = {
   boxSize: [9, 7],
-  path: new Path2D(
-    'M1.76219 5.82543L1.27219 7.10542H-0.0078125L2.61219 0.435425H4.07219L6.69219 7.10542H5.41219L4.92219 5.82543H1.76219ZM7.7207 0.435425H8.8907V7.10542H7.7207V0.435425ZM4.5918 4.79566L3.3418 1.42566L2.0918 4.79566H4.5918Z',
-  ),
+  path: (BROWSER &&
+    new Path2D(
+      'M1.76219 5.82543L1.27219 7.10542H-0.0078125L2.61219 0.435425H4.07219L6.69219 7.10542H5.41219L4.92219 5.82543H1.76219ZM7.7207 0.435425H8.8907V7.10542H7.7207V0.435425ZM4.5918 4.79566L3.3418 1.42566L2.0918 4.79566H4.5918Z',
+    )) as Path2D,
 } as const
 
 export function drawSpikeExplanation(
@@ -34,10 +36,8 @@ export function drawSpikeExplanation(
   drawCircle(ctx, coords, circleOptions)
   ctx.stroke()
 
-  if (this.state === ItemMouseState.HOVER) {
-    ctx.fillStyle = '#f2eeff'
-    ctx.fill()
-  }
+  ctx.fillStyle = this.state === ItemMouseState.HOVER ? '#f2eeff' : '#ffffff'
+  ctx.fill()
 
   ctx.fillStyle = '#8358FF'
   drawSvgIcon(ctx, coords, AI_ICON)
