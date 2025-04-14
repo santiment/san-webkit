0b9cdd2839cbee544e314164760ea9da888c8fd1
Vlad Keliy <vekkele@gmail.com>
2025-04-14 12:41:32 +0200

    feat(alert) Update alert form layout ui

--- src/lib/ui/app/Alerts/Dialog/AlertFormScreen.svelte
+++ src/lib/ui/app/Alerts/Dialog/AlertFormScreen.svelte
@@ -18,7 +18,7 @@
   // Reduce steps state.$$ to accumulated object
   function onAlertCreate() {
     const reducedAlert = createApiAlert()
-    console.log(reducedAlert)
+    console.log({ reducedAlert, isAlertValid })
   }
 
   function createApiAlert() {
@@ -28,43 +28,47 @@
   }
 </script>
 
-<section>
-  {#if !apiAlert}
-    <Button
-      variant="plain"
-      icon="arrow"
-      iconSize="8"
-      class="back fill-waterloo text-waterloo hover:fill-green hover:text-green [&>svg]:-rotate-90"
-      onclick={resetCategory}
-    >
-      Categories
-    </Button>
-  {/if}
+<section class="flex min-h-0 flex-1">
+  <aside class="relative h-full w-[374px] overflow-auto border-r pb-[100px] pl-12 pr-10 pt-8">
+    <div class="mb-6 flex items-center justify-between border-b pb-4">
+      <h2 class="text-xl">{schema.ui.label}</h2>
 
-  <h2 class="text-lg">Schema: {schema.name}</h2>
-
-  {#each steps as step, i (step.name)}
-    <div class="m-2 flex border">
-      <Button onclick={() => (selectedStep.index$ = i)}>
-        {step.name}
-      </Button>
-
-      , valid: {step.isValid.$},
-
-      {#if step.ui.Legend}
-        <step.ui.Legend {step}></step.ui.Legend>
+      {#if !apiAlert}
+        <Button
+          variant="plain"
+          icon="arrow"
+          iconSize="8"
+          class="back fill-waterloo text-waterloo hover:fill-green hover:text-green [&>svg]:-rotate-90"
+          onclick={resetCategory}
+        >
+          Categories
+        </Button>
       {/if}
     </div>
-    <hr />
-  {/each}
 
-  <div>Result validity: {isAlertValid.$}</div>
+    {#each steps as step, i (step.name)}
+      <div class="m-2 flex border">
+        <Button onclick={() => (selectedStep.index$ = i)}>
+          {step.name}
+        </Button>
 
-  <hr />
+        , valid: {step.isValid.$},
 
-  <div class="p-4">
+        {#if step.ui.Legend}
+          <step.ui.Legend {step}></step.ui.Legend>
+        {/if}
+      </div>
+      <hr />
+    {/each}
+
+    <Button variant="fill" onclick={() => onAlertCreate()}>Submit</Button>
+  </aside>
+
+  <main class="flex flex-1 flex-col overflow-auto pb-8 pl-10 pr-12 pt-9">
+    <div class="mb-4 flex justify-between">
+      <h3 class="text-lg font-medium">{selectedStep.$.ui.label}</h3>
+    </div>
+
     <selectedStep.$.ui.Form step={selectedStep.$}></selectedStep.$.ui.Form>
-  </div>
-
-  <Button variant="fill" onclick={() => onAlertCreate()}>Submit</Button>
+  </main>
 </section>
