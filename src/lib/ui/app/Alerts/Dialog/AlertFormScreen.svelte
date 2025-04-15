2ef370b8a5d24708136e0318452ab99fb2387d8e
Vlad Keliy <vekkele@gmail.com>
2025-04-15 10:55:41 +0200

    refactor(alert) Move steps nav list to separate component

--- src/lib/ui/app/Alerts/Dialog/AlertFormScreen.svelte
+++ src/lib/ui/app/Alerts/Dialog/AlertFormScreen.svelte
@@ -3,10 +3,9 @@
   import type { TApiAlert } from '../types.js'
 
   import Button from '$ui/core/Button/index.js'
-  import { cn } from '$ui/utils/index.js'
-  import Svg from '$ui/core/Svg/Svg.svelte'
 
   import { useAlertFormCtx } from '../ctx/index.svelte.js'
+  import Steps from './Steps.svelte'
 
   type TProps = {
     schema: TAlertSchemaUnion
@@ -48,52 +47,11 @@
       {/if}
     </div>
 
-    <nav>
-      {#each steps as step, i}
-        {@const { title, description } = step.ui}
-        {@const active = selectedStep.index$ === i}
-        {@const valid = step.isValid.$}
-        {@const next = !valid && (steps[i - 1] ? steps[i - 1].isValid.$ : true)}
-
-        <button
-          class={cn(
-            'pointer-events-none relative flex max-w-full pb-5 text-casper',
-            'before:absolute before:bottom-0 before:left-3 before:top-0 before:-z-10 before:block before:w-px before:bg-porcelain',
-            'last:before:hidden',
-            (next || valid) && 'pointer-events-auto text-fiord hover:text-green',
-            active && 'text-green',
-          )}
-          onclick={() => (selectedStep.index$ = i)}
-        >
-          <span
-            class={cn(
-              'mr-3 flex size-6 min-w-6 items-center justify-center rounded-full bg-mystic fill-white text-white',
-              next && 'bg-waterloo',
-              (active || valid) && 'bg-green',
-            )}
-          >
-            {#if valid}
-              <Svg id="checkmark" w="8" />
-            {:else}
-              {i + 1}
-            {/if}
-          </span>
-
-          <div class="min-w-0 text-start">
-            <h3 class="text-base">{title}</h3>
-            {#if valid && step.ui.Legend}
-              <div class="mt-2 flex text-fiord">
-                <step.ui.Legend {step} />
-              </div>
-            {:else if active}
-              <p class="text-fiord">
-                {description}
-              </p>
-            {/if}
-          </div>
-        </button>
-      {/each}
-    </nav>
+    <Steps
+      {steps}
+      selectedIndex={selectedStep.index$}
+      onStepSelect={(i) => (selectedStep.index$ = i)}
+    />
 
     <Button variant="fill" onclick={() => onAlertCreate()}>Submit</Button>
   </aside>
---
+++ src/lib/ui/app/Alerts/Dialog/Steps.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import type { TAlertStep } from '../form-steps/index.svelte.js'
+
+  import Svg from '$ui/core/Svg/Svg.svelte'
+  import { cn } from '$ui/utils/index.js'
+
+  type TProps = {
+    steps: TAlertStep[]
+    selectedIndex: number
+    onStepSelect: (index: number) => void
+  }
+
+  const { steps, selectedIndex, onStepSelect }: TProps = $props()
+</script>
+
+<nav>
+  {#each steps as step, i}
+    {@const { title, description } = step.ui}
+    {@const active = selectedIndex === i}
+    {@const valid = step.isValid.$}
+    {@const next = !valid && (steps[i - 1] ? steps[i - 1].isValid.$ : true)}
+
+    <button
+      class={cn(
+        'pointer-events-none relative flex max-w-full pb-5 text-casper',
+        'before:absolute before:bottom-0 before:left-3 before:top-0 before:-z-10 before:block before:w-px before:bg-porcelain',
+        'last:before:hidden',
+        (next || valid) && 'pointer-events-auto text-fiord hover:text-green',
+        active && 'text-green',
+      )}
+      onclick={() => onStepSelect(i)}
+    >
+      <span
+        class={cn(
+          'mr-3 flex size-6 min-w-6 items-center justify-center rounded-full bg-mystic fill-white text-white',
+          next && 'bg-waterloo',
+          (active || valid) && 'bg-green',
+        )}
+      >
+        {#if valid}
+          <Svg id="checkmark" w="8" />
+        {:else}
+          {i + 1}
+        {/if}
+      </span>
+
+      <div class="min-w-0 text-start">
+        <h3 class="text-base">{title}</h3>
+        {#if valid && step.ui.Legend}
+          <div class="mt-2 flex text-fiord">
+            <step.ui.Legend {step} />
+          </div>
+        {:else if active}
+          <p class="text-fiord">
+            {description}
+          </p>
+        {/if}
+      </div>
+    </button>
+  {/each}
+</nav>
