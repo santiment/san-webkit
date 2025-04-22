3e5169b7f73c5f05ad6a360b32492bf16b41fa5d
Vlad Keliy <vekkele@gmail.com>
2025-04-22 11:04:58 +0200

    feat(types) Add exactObjectKeys helper function

--- src/lib/utils/types/index.ts
+++ src/lib/utils/types/index.ts
@@ -1,3 +1,7 @@
 export type TNominal<GType, GName extends string> = GType & { readonly [Symbol.species]: GName }
 
 export type IfAny<T, Y, N> = 0 extends 1 & T ? Y : N
+
+export function exactObjectKeys<T extends object>(obj: T) {
+  return Object.keys(obj) as (keyof typeof obj)[]
+}
