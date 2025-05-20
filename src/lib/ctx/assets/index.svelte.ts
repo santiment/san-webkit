6b4cd80b4eb9b6216c6a03db6beaf0caad29c745
DmitriVanGuard <dimavanguard@gmail.com>
2025-05-20 11:46:10 +0300

    feat(assets): Asset by ticker map

--- src/lib/ctx/assets/index.svelte.ts
+++ src/lib/ctx/assets/index.svelte.ts
@@ -8,7 +8,10 @@ import { queryAllProjects, type TAsset, type TAssetSlug } from './api.js'
 export const useAssetsCtx = createCtx('webkit_useAssetsCtx', () => {
   let assets = $state.raw<TAsset[]>([])
 
-  const assetBySlugMap = $derived(new Map(assets.map((item) => [item.slug, item])))
+  const assetBySlugMap = $derived(
+    new Map(assets.map((item) => [item.slug.toLowerCase() as string, item])),
+  )
+  const assetByTickerMap = $derived(createAssetTickerMap(assets))
 
   onMount(() => {
     queryAllProjects(Query)().then((items) => (assets = items))
@@ -21,8 +24,29 @@ export const useAssetsCtx = createCtx('webkit_useAssetsCtx', () => {
       },
     },
 
-    getAssetBySlug(slug: string): undefined | TAsset {
-      return assetBySlugMap.get(slug as TAssetSlug)
+    getAssetBySlug(slug: undefined | null | string): undefined | TAsset {
+      return slug ? assetBySlugMap.get(slug.toLowerCase() as TAssetSlug) : undefined
+    },
+
+    /**
+     * Returns first matched ticker with highest rank
+     */
+    getAssetByTicker(ticker: undefined | null | string): undefined | TAsset {
+      return ticker ? assetByTickerMap.get(ticker.toLowerCase()) : undefined
     },
   }
 })
+
+function createAssetTickerMap(assets: TAsset[]) {
+  const map = new Map<string, undefined | TAsset>()
+
+  for (const asset of assets) {
+    const ticker = asset.ticker?.toLowerCase()
+
+    if (map.has(ticker)) continue
+
+    map.set(ticker, asset)
+  }
+
+  return map
+}
