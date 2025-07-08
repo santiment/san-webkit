8b7d5d5bc58e3019c66d2914912b7e2699034ab1
Vlad Keliy <vekkele@gmail.com>
2025-07-08 15:15:50 +0300

    fix(plan) User proper utility for checking for custom plan

--- src/ui/Pricing/SubscriptionSettings/SubscriptionCard/suggestions.ts
+++ src/ui/Pricing/SubscriptionSettings/SubscriptionCard/suggestions.ts
@@ -7,6 +7,7 @@ import {
   Plan,
   checkIsBusinessPlan,
   checkIsIndividualPlan,
+  getIsCustomPlan,
 } from '@/utils/plans'
 
 export type Suggestion = {
@@ -94,7 +95,7 @@ export function getBusinessSuggestions(userPlan: UserPlan | null): PlanSuggestio
     return allBusinessSuggestions.slice(1)
   }
 
-  const planName = userPlan.name.startsWith('CUSTOM') ? Plan.CUSTOM : (userPlan.name as Plan)
+  const planName = getIsCustomPlan(userPlan.name) ? Plan.CUSTOM : (userPlan.name as Plan)
 
   const planIndex = Array.from(BUSINESS_PLANS).indexOf(planName)
   if (planIndex === -1) return allBusinessSuggestions
