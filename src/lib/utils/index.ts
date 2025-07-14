9dfd3ce06da663c029319e9565a3c5818fa22c7c
Vlad Keliy <vekkele@gmail.com>
2025-07-14 12:17:10 +0300

    fix(cookies) Export cookies helpers for external use

--- src/lib/utils/index.ts
+++ src/lib/utils/index.ts
@@ -13,6 +13,8 @@ export const getRandomKey = (): string => Math.floor(Math.random() * 0xffffffff)
 
 export { ss, type SS } from './state.svelte.js'
 
+export { getCookie, setCookie, deleteCookie } from './cookies.js'
+
 /**
  * Designed for cases when universal page load function should have a conditional query, which runs only on app boot
  */
