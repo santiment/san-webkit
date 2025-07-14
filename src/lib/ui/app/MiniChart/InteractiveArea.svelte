b608a778142a3935d895b10dd133b7a9d63c2813
Serafim <serafim@Serafims-MacBook-Pro.local>
2025-07-14 16:26:12 +0400

    feat(mini-chart): extracted logic into extra wrapper for area component

--- src/lib/ui/app/MiniChart/InteractiveArea.svelte
+++ src/lib/ui/app/MiniChart/InteractiveArea.svelte
@@ -3,16 +3,14 @@
 
   import Area from './Area.svelte'
   import SvgTooltip from './SvgTooltip.svelte'
-  import { getOffset, getValueAt } from './utils.js'
-  import { useMiniChartTooltipCtx } from './ctx.svelte.js'
+  import { getValueAt } from './utils.js'
+  import InteractiveContainer from './InteractiveContainer.svelte'
 
   type TProps = {
     tooltipVisible?: boolean
     tooltipSyncKey?: string
     formatTooltipValue?: (value: number) => string
   } & ComponentProps<typeof Area>
-
-  const { tooltip } = useMiniChartTooltipCtx()
 
   const {
     data,
@@ -23,43 +21,18 @@
     formatTooltipValue = (value: number) => value.toString(),
     ...rest
   }: TProps = $props()
-
-  let localOffset = $state(0)
-
-  const offset = $derived(
-    getOffset({
-      localOffset,
-      sharedOffset: tooltip.$.offset,
-      tooltipSyncKey,
-      storeSyncKey: tooltip.$.syncKey,
-    }),
-  )
-  const currentValue = $derived(getValueAt(data, offset, width))
-  const valueFormatted = $derived(formatTooltipValue(currentValue))
-
-  function onMouseMove({ offsetX }: MouseEvent) {
-    localOffset = offsetX
-
-    if (tooltipSyncKey) {
-      tooltip.updateOffset(offsetX, tooltipSyncKey)
-    }
-  }
-
-  function onMouseLeave() {
-    localOffset = 0
-
-    if (tooltipSyncKey) {
-      tooltip.updateOffset(0, tooltipSyncKey)
-    }
-  }
 </script>
 
-<Area {...rest} {data} {width} {height} onmousemove={onMouseMove} onmouseleave={onMouseLeave}>
-  {#if tooltipVisible && offset}
-    <line x1={offset} x2={offset} y1="0" y2={height} class="stroke-waterloo" stroke-width="2" />
+<InteractiveContainer as={Area} {tooltipSyncKey} {data} {width} {height} {...rest}>
+  {#snippet children({ offset })}
+    {#if tooltipVisible && offset}
+      {@const currentValue = getValueAt(data, offset, width)}
+      {@const valueFormatted = formatTooltipValue(currentValue)}
+      <line x1={offset} x2={offset} y1="0" y2={height} class="stroke-waterloo" stroke-width="2" />
 
-    <foreignObject {width} {height}>
-      <SvgTooltip {valueFormatted} />
-    </foreignObject>
-  {/if}
-</Area>
+      <foreignObject {width} {height}>
+        <SvgTooltip {valueFormatted} />
+      </foreignObject>
+    {/if}
+  {/snippet}
+</InteractiveContainer>
