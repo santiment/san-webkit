d9fe561fd94b7a35db2b56fa018e9168ecff7654
Serafim <serafim@Serafims-MacBook-Pro.local>
2025-07-16 14:06:12 +0400

    feat(interactive-tour): added interactive tour with analytics

--- src/stories/Design System - App UI/InteractiveTour/index.svelte
+++ src/stories/Design System - App UI/InteractiveTour/index.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
   import Button from '$ui/core/Button/Button.svelte'
-  import { driver } from 'driver.js'
-  import 'driver.js/dist/driver.css'
+  import { interactiveTour } from '$ui/core/InteractiveTour/index.js'
 
   const ids = {
     start: 'test-start',
@@ -10,50 +9,56 @@
     feature3: 'test-feature3',
   } as const
 
-  const driverObj = driver({
-    steps: [
-      {
-        element: ids.start,
-        popover: {
-          title: 'New master select',
-          description: `<p class="mrg-l mrg--b">Paste the concrete smart contract address for fetching relevant metrics for it</p>`,
-        },
-      },
-      {
-        element: ids.feature1,
-        popover: {
-          title: 'First Feature',
-          description: `<p class="mrg-l mrg--b">Paste the concrete smart contract address for fetching relevant metrics for it</p>`,
-        },
-      },
-      {
-        element: ids.feature2,
-        popover: {
-          title: 'Second Feature',
-          description: `<p class="mrg-l mrg--b">Paste the concrete smart contract address for fetching relevant metrics for it</p>`,
-        },
-      },
-      {
-        element: ids.feature3,
-        popover: {
-          title: 'Third Feature',
-          description: `<p class="mrg-l mrg--b">Paste the concrete smart contract address for fetching relevant metrics for it</p>`,
-        },
-      },
-    ].map(({ element, ...rest }) => ({ element: '#' + element, ...rest })),
-  })
+  async function start() {
+    const id = (id: string) => `#${id}`
 
-  function start() {
-    driverObj.drive()
+    const tour = await interactiveTour({
+      steps: [
+        {
+          element: id(ids.start),
+          popover: {
+            title: 'New master select',
+            side: 'bottom',
+            description: `<p class="mrg-l mrg--b">Paste the concrete smart contract address for fetching relevant metrics for it</p>`,
+          },
+        },
+        {
+          element: id(ids.feature1),
+          popover: {
+            title: 'First Feature',
+            side: 'bottom',
+            description: `<p class="mrg-l mrg--b">2) Paste the concrete smart contract address for fetching relevant metrics for it</p>`,
+          },
+        },
+        {
+          element: id(ids.feature2),
+          popover: {
+            title: 'Second Feature',
+            side: 'bottom',
+            description: `<p class="mrg-l mrg--b">3) Paste the concrete smart contract address for fetching relevant metrics for it</p>`,
+          },
+        },
+        {
+          element: id(ids.feature3),
+          popover: {
+            title: 'Third Feature',
+            side: 'bottom',
+            description: `<p class="mrg-l mrg--b">4) Paste the concrete smart contract address for fetching relevant metrics for it</p>`,
+          },
+        },
+      ],
+    })
+
+    tour.drive()
   }
 </script>
 
 <main class="p-8">
   <Button id={ids.start} class="px-6" variant="border" onclick={start}>Test</Button>
 
-  <row class="mt-10 flex gap-10 [&>div]:rounded [&>div]:p-3 [&>div]:text-white">
+  <div class="mt-10 flex gap-10 [&>div]:rounded [&>div]:p-3 [&>div]:text-white">
     <div class="bg-green" id={ids.feature1}>Feature 1</div>
     <div class="bg-blue" id={ids.feature2}>Feature 2</div>
     <div class="bg-purple" id={ids.feature3}>Feature 3</div>
-  </row>
+  </div>
 </main>
