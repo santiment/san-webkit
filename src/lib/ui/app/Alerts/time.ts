3b53efd8685b8a53e4cf0ef64be2134deb348a25
Vlad Keliy <vekkele@gmail.com>
2025-07-16 13:04:50 +0300

    feat(alerts) Add dropdown instead of checkbox for isRepeating option

--- src/lib/ui/app/Alerts/time.ts
+++ src/lib/ui/app/Alerts/time.ts
@@ -1,10 +1,10 @@
 import { keyify } from '$lib/utils/object.js'
 
 export const TimeModifiers = keyify({
-  m: { frequencyLabel: 'Minutely', label: 'Minute(s)', name: 'minute' },
-  h: { frequencyLabel: 'Hourly', label: 'Hour(s)', name: 'hour' },
-  d: { frequencyLabel: 'Daily', label: 'Day(s)', name: 'day' },
-  w: { frequencyLabel: 'Weekly', label: 'Week(s)', name: 'week' },
+  m: { label: 'Minute(s)', name: 'minute' },
+  h: { label: 'Hour(s)', name: 'hour' },
+  d: { label: 'Day(s)', name: 'day' },
+  w: { label: 'Week(s)', name: 'week' },
 })
 
 export type TTimeModifier = keyof typeof TimeModifiers
