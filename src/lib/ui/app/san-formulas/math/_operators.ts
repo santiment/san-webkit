ef6c11565d601b4c7de9966f6a5cecafdf39d147
DmitriVanGuard <dimavanguard@gmail.com>
2025-08-10 12:24:49 +0200

    feat(formulas): Supporting log math function

--- src/lib/ui/app/san-formulas/math/_operators.ts
+++ src/lib/ui/app/san-formulas/math/_operators.ts
@@ -9,12 +9,29 @@ const pow = createBinaryMathOperator('pow', (a, b) => Math.pow(a, b))
 const unaryMinus = createUnaryMathOperator('unaryMinus', (a) => -a)
 const unaryPlus = createUnaryMathOperator('unaryPlus', (a) => +a)
 
+const log = createMathFunction('log', (a, base) => {
+  if (!base) {
+    return Math.log(a)
+  }
+
+  if (base === 2) {
+    return Math.log2(a)
+  }
+
+  if (base === 10) {
+    return Math.log10(a)
+  }
+
+  return Math.log(a) / Math.log(base)
+})
+
 export const MathOperators = {
   add,
   subtract,
   multiply,
   divide,
   pow,
+  log,
 
   unaryMinus,
   unaryPlus,
@@ -59,5 +76,35 @@ function createUnaryMathOperator<GName extends string>(
   return math.typed(name, {
     number: operation,
     Timeseries: timeseriesNumberOperation,
+  })
+}
+
+function createMathFunction<GName extends string>(
+  name: GName,
+  operation: (a: number, b?: number) => number,
+) {
+  return math.typed(name, {
+    number: (x: number) => operation(x),
+
+    Timeseries: (x: Timeseries) =>
+      new Timeseries(
+        x.values.map((value) => operation(value)),
+        x.timestamps,
+      ),
+
+    'Timeseries | number, number': (x: number | Timeseries, base: number) => {
+      if (base < 2) {
+        throw new Error('Invalid log base: ' + base)
+      }
+
+      if (x instanceof Timeseries) {
+        return new Timeseries(
+          x.values.map((value) => operation(value, base)),
+          x.timestamps,
+        )
+      }
+
+      return operation(x, base)
+    },
   })
 }
