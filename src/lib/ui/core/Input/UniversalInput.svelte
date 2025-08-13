51f00ef118dbc7e7167eb8d137585c59c8b89530
Vlad Keliy <vekkele@gmail.com>
2025-08-13 14:14:00 +0300

    feat(input) Add custom spin buttons to number input

--- src/lib/ui/core/Input/UniversalInput.svelte
+++ src/lib/ui/core/Input/UniversalInput.svelte
@@ -1,13 +1,18 @@
 <script lang="ts">
-  import type { HTMLInputAttributes, HTMLTextareaAttributes } from 'svelte/elements'
+  import type {
+    HTMLButtonAttributes,
+    HTMLInputAttributes,
+    HTMLTextareaAttributes,
+  } from 'svelte/elements'
   import type { Snippet } from 'svelte'
   import type { SS } from 'svelte-runes'
 
   import Svg from '$ui/core/Svg/index.js'
   import { cn } from '$ui/utils/index.js'
 
+  import Button from '../Button/Button.svelte'
+
   type TCommonProps = {
-    ref?: SS<undefined | null | HTMLElement>
     class?: string
     type?: 'text' | 'number' | 'email'
     placeholder?: string
@@ -20,11 +25,13 @@
 
   type TInputProps = {
     as: 'input'
+    ref?: SS<undefined | null | HTMLInputElement>
   } & HTMLInputAttributes &
     TCommonProps
 
   type TTextAreaProps = {
     as: 'textarea'
+    ref?: SS<undefined | null | HTMLTextAreaElement>
   } & HTMLTextareaAttributes &
     TCommonProps
 
@@ -44,13 +51,15 @@
     right,
     ...rest
   }: TProps = $props()
+
+  const isNumber = $derived(type === 'number')
 </script>
 
 <!-- FIXME: Fix structure after design is discussed. Currently works incorrectly with [left] and [right] added -->
 <!-- Search for [input-left-fix] to fix hacks after update -->
 <div
   class={cn(
-    'relative flex items-center rounded-md border bg-white fill-waterloo',
+    'group relative flex items-center rounded-md border bg-white fill-waterloo',
     'focus-within:border-green focus-within:fill-green hover:border-green',
     disabled && 'bg-athens text-mystic hover:border-porcelain',
     className,
@@ -71,10 +80,48 @@
     {placeholder}
     class={cn(
       'w-full rounded-md bg-transparent py-[5px] pl-2.5 pr-3 outline-none placeholder:text-waterloo',
+      '[appearance:textfield] [&::-webkit-inner-spin-button]:appearance-none [&::-webkit-outer-spin-button]:appearance-none',
       icon && 'pl-8',
       inputClass,
     )}
   />
 
+  {#if isNumber && as === 'input'}
+    {@const inputRef = ref as Extract<TProps, { as: 'input' }>['ref']}
+
+    <section
+      class={cn(
+        'absolute right-[9px] flex h-full flex-col justify-center gap-[3px] bg-[inherit]',
+        'opacity-0 transition-opacity hover:fill-waterloo group-hover:opacity-100',
+      )}
+    >
+      {@render spinButton({ onclick: () => inputRef?.$?.stepUp() })}
+      {@render spinButton({ onclick: () => inputRef?.$?.stepDown(), class: '[&>svg]:rotate-180' })}
+    </section>
+  {/if}
+
   {@render right?.()}
 </div>
+
+{#snippet spinButton({
+  onclick,
+  class: className,
+}: {
+  onclick: HTMLButtonAttributes['onclick']
+  class?: string
+})}
+  <Button
+    variant="plain"
+    size="auto"
+    icon="triangle"
+    iconSize="6"
+    iconHeight="4"
+    {onclick}
+    class={cn(
+      'w-2.5 justify-center transition-[transform,fill]',
+      'hover:scale-150 hover:fill-black',
+      'group-focus-within:fill-waterloo group-focus-within:hover:fill-black',
+      className,
+    )}
+  />
+{/snippet}
