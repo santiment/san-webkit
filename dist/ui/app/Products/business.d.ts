241efa9697377b56eaf8416aaa38b4c481d0aab2
DmitriVanGuard <dimavanguard@gmail.com>
2025-08-19 14:37:10 +0300

    [RELEASE] 8c5d3f00

---
+++ dist/ui/app/Products/business.d.ts
@@ -0,0 +1,2 @@
+import type { TProductCard } from './ProductCard.svelte';
+export declare const business: TProductCard[];
---
+++ dist/utils/object.d.ts
@@ -0,0 +1,7 @@
+import type { Prettify } from '@melt-ui/svelte/internal/types';
+type Keyified<T, GKeyProp extends string = 'key'> = {
+    [K in keyof T]: T[K] & Record<GKeyProp, K>;
+};
+export declare function keyify<T, GKeyProp extends string = 'key'>(object: T, keyName?: GKeyProp): Prettify<Keyified<T, GKeyProp>>;
+export declare function exactObjectKeys<T extends object>(obj: T): (keyof typeof obj)[];
+export {};
