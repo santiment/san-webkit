dedefe768a757dad2372a45eff3015e9a40f1e48
Vlad Keliy <vekkele@gmail.com>
2025-10-28 08:36:48 +0300

    fix(deviceCtx) Move all logic and state inside context creator fn

--- src/lib/ctx/device/index.svelte.ts
+++ src/lib/ctx/device/index.svelte.ts
@@ -1,6 +1,5 @@
 import { BROWSER } from 'esm-env'
-import { untrack } from 'svelte'
-import { ss } from 'svelte-runes'
+import { onMount } from 'svelte'
 
 import { createCtx } from '$lib/utils/index.js'
 
@@ -18,40 +17,44 @@ function getDeviceInfo(type: DeviceType) {
   return { type, isMobile, isPhone, isDesktop: !isMobile }
 }
 
-const device = ss(getDeviceInfo(DeviceType.Desktop))
+function mapWindowToDevice(): DeviceType {
+  const { innerWidth } = window
 
-function onDeviceTypeChange(deviceType: DeviceType) {
-  untrack(() => {
-    if (device.$.type === deviceType) return
+  if (innerWidth < 480) return DeviceType.PhoneXs
+  if (innerWidth < 768) return DeviceType.Phone
+  if (innerWidth < 992) return DeviceType.Tablet
+
+  return DeviceType.Desktop
+}
+
+export const useDeviceCtx = createCtx('useDeviceCtx', (deviceType?: DeviceType) => {
+  let device = $state(getDeviceInfo(deviceType ?? DeviceType.Desktop))
+
+  function onDeviceTypeChange(deviceType: DeviceType) {
+    if (device.type === deviceType) return
 
     if (BROWSER) {
-      document.body.classList.remove(device.$.type)
+      document.body.classList.remove(device.type)
       document.body.classList.add(deviceType)
     }
 
-    device.$ = getDeviceInfo(deviceType)
-  })
-}
-
-if (BROWSER) {
-  function mapWindowToDevice(): DeviceType {
-    const { innerWidth } = window
-
-    if (innerWidth < 480) return DeviceType.PhoneXs
-    if (innerWidth < 768) return DeviceType.Phone
-    if (innerWidth < 992) return DeviceType.Tablet
-
-    return DeviceType.Desktop
+    device = getDeviceInfo(deviceType)
   }
 
   const onResize = () => onDeviceTypeChange(mapWindowToDevice())
 
-  onResize()
-  window.addEventListener('resize', onResize, { passive: true })
-}
+  onMount(() => {
+    window.addEventListener('resize', onResize, { passive: true })
+    onResize()
 
-export const useDeviceCtx = createCtx('useDeviceCtx', (deviceType?: DeviceType) => {
-  if (deviceType) onDeviceTypeChange(deviceType)
+    return window.removeEventListener('resize', onResize)
+  })
 
-  return { device }
+  return {
+    device: {
+      get $() {
+        return device
+      },
+    },
+  }
 })
