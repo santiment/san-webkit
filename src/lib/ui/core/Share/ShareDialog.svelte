74fbf8db76d0bf49dbe4823bbf7a0eab32f33f62
Vlad Keliy <vekkele@gmail.com>
2025-11-14 11:28:43 +0300

    fix(share) Fix border styles collision in sanbase

--- src/lib/ui/core/Share/ShareDialog.svelte
+++ src/lib/ui/core/Share/ShareDialog.svelte
@@ -102,7 +102,9 @@
         bind:this={inputNode}
       />
       <Button
-        class={cn('h-10 min-w-[84px] text-nowrap rounded-none border-l border-solid px-3')}
+        class={cn(
+          'h-10 min-w-[84px] text-nowrap rounded-none border-0 border-l border-solid border-porcelain px-3',
+        )}
         onclick={onCopy}
         {disabled}
       >
