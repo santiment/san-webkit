120493cb6db77936fc222fafed5e307efbba676f
Vlad Keliy <vekkele@gmail.com>
2025-12-22 10:32:14 +0300

    [RELEASE] 7ef22922

--- src/lib/ui/app/LoginForm/Divider.svelte
+++
@@ -1,3 +0,0 @@
-<div class="my-[34px] flex h-0 items-center justify-center border-b text-sm text-waterloo">
-  <span class="bg-white px-2.5">or use</span>
-</div>
--- src/stories/Design System - App UI/Change/index.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-  import Change, { percentChange } from '$ui/app/Change/index.js'
-</script>
-
-<Change change={-50} />
-<Change change={50} />
-<Change change={percentChange(0.1, -0.2)} />
-<Change change={percentChange(0.5, 0.7)} />
