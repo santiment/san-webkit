b8bf4581df2065a811d02d5a614305f8a8d9d628
Vlad Keliy <vekkele@gmail.com>
2025-12-22 12:25:18 +0300

    chore(change) Add zero case to storybook

--- src/stories/Design System - App UI/Change/index.svelte
+++ src/stories/Design System - App UI/Change/index.svelte
@@ -2,7 +2,10 @@
   import Change, { percentChange } from '$ui/app/Change/index.js'
 </script>
 
-<Change change={-50} />
-<Change change={50} />
-<Change change={percentChange(0.1, -0.2)} />
-<Change change={percentChange(0.5, 0.7)} />
+<main class="flex flex-col gap-2 p-4">
+  <Change change={-50} />
+  <Change change={50} />
+  <Change change={percentChange(0.1, -0.2)} />
+  <Change change={percentChange(0.5, 0.7)} />
+  <Change change={0} />
+</main>
