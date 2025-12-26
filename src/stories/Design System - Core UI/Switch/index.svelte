9077ce82773c1d1aac67f7557b05f1dd3b593ccc
Vlad Keliy <vekkele@gmail.com>
2025-12-26 11:47:29 +0300

    chore(switch) Add more states to storybook

--- src/stories/Design System - Core UI/Switch/index.svelte
+++ src/stories/Design System - Core UI/Switch/index.svelte
@@ -1,7 +1,25 @@
 <script lang="ts">
-  import Switch from '$ui/core/Switch/Switch.svelte'
+  import Switch from '$ui/core/Switch/index.js'
 </script>
 
-<main class="mx-auto flex h-screen max-w-[500px] column center">
-  <Switch />
+<main class="flex flex-col gap-6 px-6 py-10">
+  <section>
+    <h3>Off - Normal</h3>
+    <Switch />
+  </section>
+
+  <section>
+    <h3>On - Normal</h3>
+    <Switch checked />
+  </section>
+
+  <section>
+    <h3>Off - Disabled</h3>
+    <Switch disabled />
+  </section>
+
+  <section>
+    <h3>On - Disabled</h3>
+    <Switch checked disabled />
+  </section>
 </main>
