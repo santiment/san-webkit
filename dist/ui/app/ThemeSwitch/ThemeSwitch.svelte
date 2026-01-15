ee68330632913c94cd7c608e02f32aefbdec5c37
DmitriVanGuard <dimavanguard@gmail.com>
2026-01-15 17:41:30 +0100

    [RELEASE] 0b77b6bc

---
+++ dist/ui/app/ThemeSwitch/ThemeSwitch.svelte
@@ -0,0 +1,62 @@
+<script lang="ts">
+  import { Switch } from 'bits-ui'
+  import { Tween } from 'svelte/motion'
+
+  import { useUiCtx } from '../../../ctx/ui/index.svelte.js'
+  import { sleep } from '../../../utils/index.js'
+  import Svg from '../../core/Svg/index.js'
+  import { cn } from '../../utils/index.js'
+
+  type TProps = {
+    class?: string
+  }
+
+  const { class: className }: TProps = $props()
+
+  const { ui } = useUiCtx()
+
+  const ANIMATION_DURATION = 200
+  const opacity = new Tween(1, { duration: ANIMATION_DURATION / 2 })
+
+  let checked = $derived(ui.$$.isNightMode)
+  let internalNightMode = $derived(ui.$$.isNightMode)
+
+  async function onCheckedChange(value: boolean) {
+    await opacity.set(0)
+    internalNightMode = value
+    await opacity.set(1)
+
+    await sleep(100)
+    ui.toggleNightMode()
+  }
+</script>
+
+<Switch.Root
+  bind:checked
+  class={cn(
+    'flex w-[72px] select-none items-center justify-between rounded-full bg-athens-day px-2.5 py-1.5 transition-colors hover:bg-porcelain-day',
+    internalNightMode && 'bg-porcelain-night hover:bg-mystic-night',
+    internalNightMode !== ui.$$.isNightMode && 'pointer-events-none',
+    className,
+  )}
+  {onCheckedChange}
+>
+  <span
+    style:transition-duration="{ANIMATION_DURATION}ms"
+    style:opacity={opacity.current}
+    class={cn(
+      'text-fiord-day transition-transform',
+      checked && 'translate-x-5',
+      internalNightMode && 'text-fiord-night',
+    )}
+  >
+    {internalNightMode ? 'Night' : 'Day'}
+  </span>
+
+  <Switch.Thumb
+    style="opacity: {opacity.current};transition-duration: {ANIMATION_DURATION}ms"
+    class={cn('transition-transform', checked && '-translate-x-[36px]')}
+  >
+    <Svg id={internalNightMode ? 'moon' : 'sun'} illus w="20" />
+  </Switch.Thumb>
+</Switch.Root>
---
+++ dist/ui/core/Calendar/CalendarBody.svelte
@@ -0,0 +1,107 @@
+<script lang="ts">
+  import type { DateValue } from '@internationalized/date'
+  import type { Month } from 'bits-ui'
+
+  import { Calendar, RangeCalendar } from 'bits-ui'
+
+  import { cn } from '../../utils/index.js'
+
+  type Props = {
+    range?: boolean
+    months: Month<DateValue>[]
+    weekdays: string[]
+  }
+
+  let { range = false, months, weekdays }: Props = $props()
+
+  const Base = range ? RangeCalendar : Calendar
+</script>
+
+<div
+  class="month flex space-x-4 space-y-0 px-3 pb-3 pt-4 sm:px-5 sm:pb-0 sm:pt-6 sm:text-base xs:flex-col xs:space-y-4"
+>
+  {#each months as month}
+    <Base.Grid class="w-full border-collapse select-none space-y-1">
+      <Base.GridHead>
+        <Base.GridRow class="mb-1 flex justify-between">
+          {#each weekdays as day}
+            <Base.HeadCell
+              class="w-8 rounded-md text-xs font-normal uppercase text-waterloo sm:text-sm"
+            >
+              {day.slice(0, 2)}
+            </Base.HeadCell>
+          {/each}
+        </Base.GridRow>
+      </Base.GridHead>
+      <Base.GridBody>
+        {#each month.weeks as weekDates}
+          <Base.GridRow class="flex w-full sm:justify-between">
+            {#each weekDates as date}
+              <Base.Cell
+                {date}
+                month={month.value}
+                class="relative m-0 size-8 p-0 text-center focus-within:z-20 sm:size-10"
+              >
+                <Base.Day
+                  class={cn(
+                    'relative inline-flex size-full items-center justify-center whitespace-nowrap rounded',
+                    'hover:bg-athens hover:text-green dark:hover:bg-porcelain',
+                    'data-[unavailable]:text-mystic data-[unavailable]:line-through',
+                    'data-[disabled]:pointer-events-none data-[disabled]:text-mystic',
+                    'data-[outside-month]:pointer-events-none',
+                    'data-[highlighted]:rounded-none data-[highlighted]:bg-green-light-1',
+                    range &&
+                      'data-[selected]:[&:not([data-selection-start])]:[&:not([data-selection-end])]:rounded-none',
+                  )}
+                />
+              </Base.Cell>
+            {/each}
+          </Base.GridRow>
+        {/each}
+      </Base.GridBody>
+    </Base.Grid>
+  {/each}
+</div>
+
+<style>
+  .month {
+    & :global([data-today]:not([data-highlighted]):not([data-selected]):not(:hover)) {
+        --tw-text-opacity: 1;
+        color: rgb(var(--c-blue) / var(--tw-text-opacity))
+    }
+
+    & :global([data-selected]) {
+        border-radius: 0.25rem
+    }
+
+    & :global([data-selected]) {
+        --tw-bg-opacity: 1;
+        background-color: rgb(var(--c-green-light-1) / var(--tw-bg-opacity))
+    }
+
+    & :global([data-selected]) {
+        --tw-text-opacity: 1;
+        color: rgb(var(--c-green) / var(--tw-text-opacity))
+    }
+
+    & :global([data-selection-start]) {
+        border-start-end-radius: 0px;
+        border-end-end-radius: 0px
+    }
+
+    & :global([data-selection-start]) {
+        border-start-start-radius: 0.25rem;
+        border-end-start-radius: 0.25rem
+    }
+
+    & :global([data-selection-end]) {
+        border-start-end-radius: 0.25rem;
+        border-end-end-radius: 0.25rem
+    }
+
+    & :global([data-selection-end]) {
+        border-start-start-radius: 0px;
+        border-end-start-radius: 0px
+    }
+  }
+</style>
