f9c0dbbd99228b5db1b0a773d9028c83e4d2b1bd
DmitriVanGuard <dimavanguard@gmail.com>
2026-03-18 12:17:53 +0100

    [RELEASE] 217d09b1

---
+++ dist/ui/app/LoginForm/SignUp.svelte.d.ts
@@ -0,0 +1,6 @@
+type $$ComponentProps = {
+    title: string;
+};
+declare const SignUp: import("svelte").Component<$$ComponentProps, {}, "">;
+type SignUp = ReturnType<typeof SignUp>;
+export default SignUp;
--- src/lib/ui/app/InsightCard/consts.ts
+++
@@ -1,1 +0,0 @@
-export const SANTIMENT_IDS = new Set([6, 7, 1269, 3031, 4522, 79570])
