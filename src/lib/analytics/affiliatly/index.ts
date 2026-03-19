2aeadf153f570f0e1ee1bfe784ab3d8549ce90d0
Serafim Gavrilov <serafim.g@santiment.net>
2026-03-19 20:54:11 -0300

    fix(analytics): load affiliatly script immediately if document is already complete

--- src/lib/analytics/affiliatly/index.ts
+++ src/lib/analytics/affiliatly/index.ts
@@ -3,9 +3,14 @@ import { newHeadScript } from '../utils.js'
 export function initAffiliatly(id: string = 'AF-1074422') {
   if (process.env.IS_LOGGING_ENABLED) return
 
-  window.addEventListener('load', () => {
+  const load = () =>
     newHeadScript(undefined, {
       src: `https://static.affiliatly.com/v3/affiliatly.js?affiliatly_code=${id}`,
     })
-  })
+
+  if (document.readyState === 'complete') {
+    load()
+  } else {
+    window.addEventListener('load', load)
+  }
 }
