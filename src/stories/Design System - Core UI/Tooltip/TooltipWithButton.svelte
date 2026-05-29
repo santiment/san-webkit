49ca433f1ae323c064c3740052de3902ab239cc2
Vlad Keliy <vekkele@gmail.com>
2026-05-29 12:08:20 +0300

    fix(tooltip) Close tooltip when click outside of its content by default, add prop to invert this behavior

---
+++ src/stories/Design System - Core UI/Tooltip/TooltipWithButton.svelte
@@ -0,0 +1,35 @@
+<script>
+  import Button from '$ui/core/Button/Button.svelte'
+  import Tooltip from '$ui/core/Tooltip/Tooltip.svelte'
+</script>
+
+<main class="flex justify-center gap-4 py-40">
+  <Tooltip closeOnOutsideClick={false} position="bottom-start">
+    {#snippet children({ ref })}
+      <Button variant="border" {ref}>Open Tooltip</Button>
+    {/snippet}
+
+    {#snippet content()}
+      <section class="max-w-64">
+        Pass [closeOnOutsideClick={false}] to keep the tooltip open when clicking outside its
+        content.
+      </section>
+    {/snippet}
+  </Tooltip>
+
+  <Button variant="fill">Some Button</Button>
+
+  <Tooltip position="bottom-end">
+    {#snippet children({ ref })}
+      <Button variant="border" {ref}>Open Tooltip</Button>
+    {/snippet}
+
+    {#snippet content()}
+      <section class="max-w-64">
+        This tooltip’s content extends to the left and can sit behind the button next to it. If that
+        button opens a popover, the tooltip should close by default when clicking this button or
+        anywhere else outside the actual tooltip content.
+      </section>
+    {/snippet}
+  </Tooltip>
+</main>
