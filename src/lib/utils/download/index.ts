9ae0dcf62ca876261b79ce51af4233568810ab4d
Vlad Keliy <vekkele@gmail.com>
2026-07-31 10:34:36 +0300

    fix(download) Attempt to fix download on mobile safari

--- src/lib/utils/download/index.ts
+++ src/lib/utils/download/index.ts
@@ -4,10 +4,11 @@ export function downloadBlob(blob: Blob, filename: string) {
   const a = document.createElement('a')
   a.href = url
   a.download = filename
+  document.body.appendChild(a)
   a.click()
-  a.remove()
 
   setTimeout(() => {
+    a.remove()
     URL.revokeObjectURL(url)
   }, 30_000)
 }
