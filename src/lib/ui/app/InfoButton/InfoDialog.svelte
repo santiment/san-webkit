80b1050d82ba8b92bc4bd7293a9db0a679f84885
Vlad Keliy <vekkele@gmail.com>
2026-08-05 11:25:02 +0300

    feat(info button) Add info button component

---
+++ src/lib/ui/app/InfoButton/InfoDialog.svelte
@@ -0,0 +1,36 @@
+<script lang="ts" module>
+  import { dialogs$ } from '$lib/ui/core/Dialog/index.js'
+
+  import Component from './InfoDialog.svelte'
+
+  export const showInfoDialog$ = () => dialogs$.new(Component)
+</script>
+
+<script lang="ts">
+  import type { Snippet } from 'svelte'
+
+  import Dialog, { type TDialogProps } from '$lib/ui/core/Dialog/index.js'
+  import Button from '$lib/ui/core/Button/index.js'
+  import { cn } from '$ui/utils/index.js'
+
+  type TProps = {
+    class?: string
+    title: string
+    children: Snippet
+  } & TDialogProps
+
+  const { class: className, title, children, Controller }: TProps = $props()
+</script>
+
+<Dialog class="h-auto">
+  <h2
+    class="flex items-center justify-between border-b bg-athens px-5 py-2 pr-3 text-base text-fiord"
+  >
+    {title}
+    <Button icon="close" iconSize="12" onclick={() => Controller.close()} />
+  </h2>
+
+  <section class={cn('px-5 py-4 text-base', className)}>
+    {@render children()}
+  </section>
+</Dialog>
---
+++ src/lib/ui/app/InfoButton/index.svelte
@@ -0,0 +1,60 @@
+<script lang="ts">
+  import type { ComponentProps, Snippet } from 'svelte'
+
+  import { useDeviceCtx } from '$lib/ctx/device/index.svelte.js'
+  import Popover from '$ui/core/Popover/Popover.svelte'
+  import Button from '$ui/core/Button/Button.svelte'
+  import { cn } from '$ui/utils/index.js'
+
+  import { showInfoDialog$ } from './InfoDialog.svelte'
+
+  type TProps = {
+    class?: string
+    title?: string
+    contentClass?: string
+    triggerProps?: Omit<ComponentProps<typeof Button>, 'onclick' | 'class'>
+    children: Snippet
+  }
+
+  const {
+    class: className,
+    contentClass,
+    title = 'Info',
+    triggerProps,
+    children: infoContent,
+  }: TProps = $props()
+
+  const { device } = useDeviceCtx.get()
+  const showInfoDialog = showInfoDialog$()
+
+  let isPopoverOpened = $state(false)
+</script>
+
+{#if device.$.isPhone}
+  {@render trigger({
+    onclick: () => showInfoDialog({ title, class: contentClass, children: infoContent }),
+    ...triggerProps,
+  })}
+{:else}
+  <Popover bind:isOpened={isPopoverOpened} class="p-0" openOnHover>
+    {#snippet children({ props })}
+      {@render trigger({ ...props, ...triggerProps })}
+    {/snippet}
+
+    {#snippet content()}
+      <section class={cn('max-w-60 px-5 py-4 text-sm', contentClass)}>
+        {@render infoContent()}
+      </section>
+    {/snippet}
+  </Popover>
+{/if}
+
+{#snippet trigger(props: ComponentProps<typeof Button>)}
+  <Button
+    size="sm"
+    icon="info"
+    iconSize="12"
+    class={cn(isPopoverOpened && 'bg-[var(--ghost-active-bg)', className)}
+    {...props}
+  />
+{/snippet}
