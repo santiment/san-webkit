65bec3eef6f593ead53926ec046075bb623de59e
Vlad Keliy <vekkele@gmail.com>
2026-08-20 09:51:45 +0300

    feat(responsive) Add optional css mode to OnlyOnDevice utlity

--- src/lib/ui/utils/OnlyOnDevice/OnlyOnDevice.svelte
+++ src/lib/ui/utils/OnlyOnDevice/OnlyOnDevice.svelte
@@ -3,18 +3,33 @@
 
   import { useDeviceCtx } from '$lib/ctx/device/index.svelte.js'
 
-  let {
-    desktop = false,
-    tablet = false,
-    phone = false,
-    children,
-  }: { desktop?: boolean; tablet?: boolean; phone?: boolean; children: Snippet } = $props()
+  import { cn } from '../index.js'
+
+  type TProps = {
+    css?: boolean
+    desktop?: boolean
+    tablet?: boolean
+    phone?: boolean
+    children: Snippet
+  }
+
+  const { css = false, desktop = false, tablet = false, phone = false, children }: TProps = $props()
 
   const targets = { desktop, tablet, phone, 'phone-xs': phone }
 
   const { device } = useDeviceCtx()
 </script>
 
-{#if targets[device.$.type]}
+{#if css}
+  <div
+    class={cn(
+      desktop ? 'contents' : 'hidden',
+      tablet ? 'md:contents' : 'md:hidden',
+      phone ? 'sm:contents' : 'sm:hidden',
+    )}
+  >
+    {@render children()}
+  </div>
+{:else if targets[device.$.type]}
   {@render children()}
 {/if}
