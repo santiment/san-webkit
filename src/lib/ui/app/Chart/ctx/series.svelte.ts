f4396482837575ed74684eb477a3bc371ad65689
Vlad Keliy <vekkele@gmail.com>
2026-09-11 09:52:10 +0300

    refactor(series) Fix series ctx findById types

--- src/lib/ui/app/Chart/ctx/series.svelte.ts
+++ src/lib/ui/app/Chart/ctx/series.svelte.ts
@@ -488,8 +488,8 @@ export const useMetricSeriesCtx = createCtx(
           this.delete(index)
         },
 
-        findById(id?: string) {
-          return id && series.find((item) => item.id === id)
+        findById(id: string | undefined) {
+          return id ? series.find((item) => item.id === id) : undefined
         },
       },
     }
