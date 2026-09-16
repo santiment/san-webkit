70cdd11acd29b8e6f571782f3304cd9ae5c75c7a
Vlad Keliy <vekkele@gmail.com>
2026-09-16 10:36:47 +0300

    feat(analytics) Add PageType from legacy webkit

--- src/lib/analytics/utils.ts
+++ src/lib/analytics/utils.ts
@@ -17,3 +17,20 @@ export function newHeadScript(
 
   return script
 }
+
+export enum PageType {
+  EXPLORER = 'explorer',
+  CHARTS = 'charts',
+  DASHBOARDS = 'dashboards',
+  WATCHLIST = 'watchlist',
+  SCREENER = 'screener',
+  ALERTS = 'alerts',
+  INSIGHTS = 'insights',
+  PROFILE = 'profile',
+  ACCOUNT = 'account',
+  SOCIAL_TOOL = 'social_tool',
+  HISTORICAL_BALANCE = 'historical_balance',
+  LABS = 'labs',
+  SignUp = 'signup',
+  Login = 'login',
+}
